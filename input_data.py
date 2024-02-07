cfd3f13dc9471e9fb8fdffe955d50145a37f5120
DK96-OS <69859316+DK96-OS@users.noreply.github.com>
2024-02-07 15:15:23 -0500

    Data: *u input_data.py - make member data Path objects, remove method get_data_dir_path

--- input_data.py
+++ input_data.py
@@ -12,24 +12,17 @@ class InputData:
     - data_dir (str, optional): The path to the data directory.
     """
 
-    tree_file: str
-    data_dir: Optional[str]
+    tree_file: Path
+    data_dir: Optional[Path]
 
     def get_tree_input(self) -> str:
         """Obtain the String contents of the Tree File."""
-        tree_path = Path(self.tree_file)
-        if not tree_path.exists():
+        if not self.tree_file.exists():
             raise IOError("Tree File does not exist.")
         try:
-            return tree_path.read_text()
+            return self.tree_file.read_text()
         except:
             raise IOError("Failed to Read from Tree File.")
-
-    def get_data_dir_path(self) -> Optional[Path]:
-        """Obtain a Path to the Data Directory."""
-        if self.data_dir is None:
-            return None
-        return Path(self.data_dir)
 
 
 def get_input_from_args(arguments: Optional[list[str]]) -> Optional[InputData]:
