fea096a752573e16f66dddd575dbab0a0260aae0
DK96-OS <69859316+DK96-OS@users.noreply.github.com>
2024-02-24 17:31:15 -0500

    Tests: * Move all tests into test package

---
+++ test/test_argument_validation.py
@@ -0,0 +1,40 @@
+import unittest
+from src.argument_validation import ArgumentValidation
+
+
+class TestArgumentValidation(unittest.TestCase):
+
+    def setUp(self):
+        self.instance = ArgumentValidation()
+
+    def test_parse_args_no_args(self):
+        self.assertFalse(
+            self.instance.parse_args()
+        )
+
+    def test_parse_args_empty_args(self):
+        self.assertFalse(
+            self.instance.parse_args([])
+        )
+
+    def test_parse_args_single_arg(self):
+        file_name = 'tree-struct'
+        self.assertTrue(
+            self.instance.parse_args([file_name])
+        )
+        self.assertEqual(
+            file_name, str(self.instance.get_input().tree_file)
+        )
+
+    def test_parse_args_two_args(self):
+        file_name = 'tree-struct'
+        data_dir = "docs"
+        self.assertTrue(
+            self.instance.parse_args([file_name, '--data_dir=' + data_dir])
+        )
+        self.assertEqual(
+            file_name, str(self.instance.get_input().tree_file)
+        )
+        self.assertEqual(
+            data_dir, str(self.instance.get_input().data_dir)
+        )
---
+++ test/test_data_directory.py
@@ -0,0 +1,65 @@
+import unittest
+from data_directory import get_file_extension, is_valid_data_label, DataDirectory
+
+
+class TestDataDirectory(unittest.TestCase):
+
+    def setUp(self):
+        pass
+
+    def test_get_file_extension_empty_str_returns_none(self):
+        self.assertIsNone(
+            get_file_extension("")
+        )
+
+    def test_get_file_extension_space_char_returns_none(self):
+        self.assertIsNone(
+            get_file_extension(" ")
+        )
+    
+    def test_get_file_extension_name_only_returns_none(self):
+        self.assertIsNone(
+            get_file_extension("name")
+        )
+    
+    def test_get_file_extension_java_file_returns_java(self):
+        self.assertEqual(
+            "java",
+            get_file_extension("ClassName.java")
+        )
+    
+    def test_get_file_extension_python_file_returns_py(self):
+        self.assertEqual(
+            "py",
+            get_file_extension("python_script.py")
+        )
+    
+    def test_is_valid_data_label_empty_str_returns_false(self):
+        self.assertFalse(
+            is_valid_data_label("")
+        )
+    
+    def test_is_valid_data_label_space_char_returns_false(self):
+        self.assertFalse(
+            is_valid_data_label(" ")
+        )
+    
+    def test_is_valid_data_label_name_returns_true(self):
+        self.assertTrue(
+            is_valid_data_label("Name")
+        )
+    
+    def test_is_valid_data_label_file_with_ext_returns_true(self):
+        self.assertTrue(
+            is_valid_data_label("ClassName.java")
+        )
+    
+    def test_is_valid_data_label_file_with_numbers_returns_true(self):
+        self.assertTrue(
+            is_valid_data_label("FileNumber23")
+        )
+    
+    def test_is_valid_data_label_file_with_number_and_ext_returns_true(self):
+        self.assertTrue(
+            is_valid_data_label("FileNumber23.txt")
+        )
