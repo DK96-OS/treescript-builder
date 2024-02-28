5e04dd61f603f04b4f1f68b9917d9a8787ebcb16
DK96-OS <69859316+DK96-OS@users.noreply.github.com>
2024-02-28 07:09:53 -0500

    Main: * Remove ftr * Remove src dir *u __main__.py

--- ftb/__main__.py
+++ ftb/__main__.py
@@ -1,27 +1,32 @@
-"""File Tree Builder
+"""File Tree Builder Main Startup Script.
 """
 
 
 def main():
-    """Validates Arguments, then runs File Tree Builder."""
+    """
+    Validates Arguments, then runs File Tree Builder.
+    """
     import sys
-    import input
-    import src.input_line_reader
-    from src.tree_worker import TreeWorker
+    from input import validate_input_arguments, read_input_instructions
+    from tree.tree_worker import TreeWorker
     # Validate CL Arguments
-    input_data = input.validate_input_arguments(sys.argv)
-    # Process all Lines of text into procedural data
-    procedural_data_sequence = src.input_line_reader.process_many_lines(input_data.tree_input)
-    if len(procedural_data_sequence) < 1:
-        raise SystemExit("Empty Procedure")
+    input_data = validate_input_arguments(sys.argv)
     # Use this Worker to execute Stateful Procedural Operations
     worker = TreeWorker(input_data.data_dir)
-    # Process one line at a time
-    for data in procedural_data_sequence:
-        if not worker.build(data):
-            raise Exception(
-                "Invalid Operation: name=" + data.name + ", depth=" + data.depth
-            )
+    # Is Builder or Reverse
+    if input_data.is_reversed:
+        for data in read_input_instructions(input_data.tree_input):
+            if not worker.remove(data):
+                raise Exception(
+                    "Invalid Operation: name=" + data.name + ", depth=" + data.depth
+                )
+        worker.cleanup_path_stack()
+    else:
+        for data in read_input_instructions(input_data.tree_input):
+            if not worker.build(data):
+                raise Exception(
+                    "Invalid Operation: name=" + data.name + ", depth=" + data.depth
+                )
 
 
 if __name__ == "__main__":
