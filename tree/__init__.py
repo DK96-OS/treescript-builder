3a61d356501ce7bf5ff6e215c7a8bd250ca94eab
DK96-OS <69859316+DK96-OS@users.noreply.github.com>
2024-03-02 07:47:45 -0500

    Tree: *u __init__.py - add method validate_tree_data

--- tree/__init__.py
+++ tree/__init__.py
@@ -1,1 +1,14 @@
-"""The Tree Module"""
+"""The Tree Module.
+"""
+from data.tree_data import TreeData
+from tree.path_stack import PathStack
+
+
+def validate_tree_data(tree_data: tuple[TreeData]):
+    """
+    """
+    stack = PathStack()
+    #todo: Run through the tree data
+
+    #todo: Compress Directory Operations
+
