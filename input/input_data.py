63ed7faacbc934f30d9cdc20b8467c027cf13552
DK96-OS <69859316+DK96-OS@users.noreply.github.com>
2024-03-03 04:31:46 -0500

    Data: * Move input_data.py to Input package * Move tree_data.py to Input Package

--- input/input_data.py
+++ input/input_data.py
@@ -1,8 +1,10 @@
 """Valid Input Data Class.
 """
-from typing import Optional
+from typing import Optional, Generator
 from dataclasses import dataclass
 from pathlib import Path
+
+from input.tree_data import TreeData
 
 
 @dataclass(frozen=True)
@@ -18,3 +20,14 @@ class InputData:
     tree_input: str
     data_dir: Optional[Path]
     is_reversed: bool
+
+
+    def get_tree_data(self) -> Generator[TreeData, None, None]:
+        """
+        Initializes a Generator for processing the Tree Input.
+
+        Returns:
+        Generator - Yields one TreeData for each Line of Tree Input.
+        """
+        from line_reader import read_input_tree
+        return read_input_tree(self.tree_input)
