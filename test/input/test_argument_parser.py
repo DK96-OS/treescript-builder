5d8fb9922a93c127cd17a7c75076abcfeb034a5b
DK96-OS <69859316+DK96-OS@users.noreply.github.com>
2024-03-03 04:49:27 -0500

    Test: * Rename test_argument_parsing to test_argument_parser *u test_argument_parser.py - ensure that reverse operations have a directory *u __init__.py

--- test/input/test_argument_parser.py
+++ test/input/test_argument_parser.py
@@ -1,9 +1,11 @@
 """Testing Argument Parsing Module.
 """
 import pytest
-from input.argument_parsing import parse_arguments
+from input.argument_data import ArgumentData
+from input.argument_parser import parse_arguments
 
 
+@pytest.mark.xfail(raises=SystemExit, reason="invalid arguments")
 @pytest.mark.parametrize(
     "test_input",
     [
@@ -13,28 +15,25 @@ from input.argument_parsing import parse_arguments
         (["--data"]),
         (["-f"]),
         (["tree_file", "--data_dir="]),
+        (["tree_file", "-r"]),
+        (["tree_file", "--reverse"]),
     ]
 )
 def test_parse_arguments_raises_value_error(test_input):
-    try:
+    with pytest.raises(SystemExit) as exit_info:
         parse_arguments(test_input)
-    except ValueError as E:
-        assert True
-    finally:
-        assert False
+    assert exit_info.type == SystemExit
 
 
 @pytest.mark.parametrize(
     "test_input,expect",
     [
         (["tree_file"], ArgumentData("tree_file", None, False)),
-        (["tree_file", "-r"], ArgumentData("tree_file", None, True),
-        (["tree_file", "--reversed"], ArgumentData("tree_file", None, True),
         (["tree_file"], ArgumentData("tree_file", None, False)),
         (["tree_file", "--data_dir=data"], ArgumentData("tree_file", "data", False)),
         (["tree_file", "--data_dir=data", "-r"], ArgumentData("tree_file", "data", True)),
         (["tree_file", "--data_dir=data", "-r"], ArgumentData("tree_file", "data", True)),
     ]
 )
-def test_parse_arguments_raises_value_error(test_input, expect):
+def test_parse_arguments_returns_data(test_input, expect):
     assert parse_arguments(test_input) == expect
