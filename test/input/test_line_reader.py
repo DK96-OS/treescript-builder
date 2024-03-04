7b4c5e73c64bdf96a685a3a8f0ca7877c5e2e171
DK96-OS <69859316+DK96-OS@users.noreply.github.com>
2024-03-04 16:28:39 -0500

    Test: *u test_line_reader.py - add test cases, remove unittest cases

--- test/input/test_line_reader.py
+++ test/input/test_line_reader.py
@@ -1,13 +1,16 @@
-import unittest
 import pytest
 from input.line_reader import _calculate_depth, _process_line
 from input.tree_data import TreeData
 from test.input import create_depth
 
 
+# Directory Variants: A tuple of all possible ways that a directory may be represented.
+dir_variants = ('/dir', 'dir/', '\\dir', 'dir\\')
+
+
 @pytest.mark.parametrize(
     "test_input,expect", 
-    [ 
+    [
         (create_depth(depth) + 'file', depth)
         for depth in range(0, 9)
     ]
@@ -15,12 +18,25 @@ from test.input import create_depth
 def test_calculate_depth_file_returns_true(test_input, expect):
     assert _calculate_depth(test_input) == expect
 
-# Directory Variants: A tuple of all possible ways that a directory may be represented.
-dir_variants = ('/dir', 'dir/', '\\dir', 'dir\\')
+
+@pytest.mark.parametrize(
+    "test_input", 
+    [
+        (create_depth(depth) + ' file')
+        for depth in range(0, 9)
+    ]
+)
+def test_calculate_depth_file_odd_spaces_raises_exit(test_input):
+    try:
+        _calculate_depth(test_input)
+        assert False
+    except ValueError as e:
+        assert True
+
 
 @pytest.mark.parametrize(
     "test_input,expect", 
-    [ 
+    [
         (create_depth(depth) + dir, depth)
         for dir in dir_variants
         for depth in range(0, 9)
@@ -30,75 +46,98 @@ def test_calculate_depth_dir_returns_true(test_input, expect):
     assert _calculate_depth(test_input) == expect
 
 
-class TestLineReader(unittest.TestCase):
-
-    def setUp(self):
-        """ Create Data Structures For Tests
-        Directory Variants (dir_variants) : 
-            A tuple of all possible ways that a directory may be represented.
-        """
-        self.dir_variants = ('/dir', 'dir/', '\\dir', 'dir\\')
-
-    def test_calculate_depth_file(self):
-        for depth in range(0, 15):
-            self.assertEqual(_calculate_depth(create_depth(depth) + 'file'), depth)
-
-    def test_create_depth_space_char_3(self):
-        for depth in range(0, 15):
-            self.assertEqual(_calculate_depth(create_depth(depth) + 'file'), depth)
-
-    def test_calculate_depth_dir_variants(self):
-        for var in self.dir_variants:
-            for depth in range(0, 15):
-                input_line = create_depth(depth) + var
-                self.assertEqual(
-                    _calculate_depth(input_line), depth
-                )
-    
-    def test_process_line_file_(self):
-        file_name = 'file'
-        for depth in range(3):
-            input_line = create_depth(depth) + file_name
-            self.assertEqual(
-                _process_line(input_line),
-                TreeData(depth, False, file_name, "")
-            )
-    
-    def test_process_line_dir_(self):
-        for dir_path in self.dir_variants:
-            for depth in range(15):
-                input_line = create_depth(depth) + dir_path
-                dir_name = dir_path.strip('/\\')
-                self.assertEqual(
-                    _process_line(input_line),
-                    TreeData(depth, True, dir_name, "")
-                )
-
-    def test_depth_0_dir_variants(self):
-        for var in self.dir_variants:
-            self.assertEqual(_calculate_depth(var), 0)
-
-    def test_depth_1_dir_variants(self):
-        for var in self.dir_variants:
-            self.assertEqual(_calculate_depth("  " + var), 1)
-
-    def test_depth_1_file(self):
-        self.assertEqual(_calculate_depth("  file1"), 1)
-
-    def test_depth_2_dir_variants(self):
-        for var in self.dir_variants:
-            self.assertEqual(_calculate_depth("    " + var), 2)
-
-    def test_depth_2_file(self):
-        self.assertEqual(_calculate_depth("    file2"), 2)
-
-    def test_depth_3_dir_variants(self):
-        for var in self.dir_variants:
-            self.assertEqual(_calculate_depth("      " + var), 3)
-
-    def test_depth_3_file(self):
-        self.assertEqual(_calculate_depth("      file3"), 3)
+@pytest.mark.parametrize(
+    "test_input", 
+    [
+        (create_depth(depth) + ' ' + dir)
+        for dir in dir_variants
+        for depth in range(0, 9)
+    ]
+)
+def test_calculate_depth_dir_odd_spaces_raises_exit(test_input):
+    try:
+        _calculate_depth(test_input)
+        assert False
+    except ValueError as e:
+        assert True
 
 
-if __name__ == '__main__':
-    unittest.main()
+@pytest.mark.parametrize(
+    "test_input,expect", 
+    [
+        (create_depth(depth) + 'file', TreeData(depth, False, 'file', ''))
+        for depth in range(0, 4)
+    ]
+)
+def test_process_line_file_returns_data(test_input, expect):
+    assert _process_line(test_input) == expect
+
+
+@pytest.mark.parametrize(
+    "test_input", 
+    [
+        (create_depth(depth) + ' ' + 'file')
+        for depth in range(0, 4)
+    ]
+)
+def test_process_line_file_odd_spaces_raises_exit(test_input):
+    try:
+        _process_line(test_input)
+        assert False
+    except ValueError as e:
+        assert True
+
+
+@pytest.mark.parametrize(
+    "test_input,expect", 
+    [
+        (create_depth(depth) + 'file DataLabel', TreeData(depth, False, 'file', 'DataLabel'))
+        for depth in range(0, 4)
+    ]
+)
+def test_process_line_file_data_label_returns_data(test_input, expect):
+    assert _process_line(test_input) == expect
+
+
+@pytest.mark.parametrize(
+    "test_input,expect", 
+    [
+        (create_depth(depth) + 'file ', TreeData(depth, False, 'file', ''))
+        for depth in range(0, 4)
+    ]
+)
+def test_process_line_file_trailing_space_returns_data(test_input, expect):
+    assert _process_line(test_input) == expect
+
+
+@pytest.mark.parametrize(
+    "test_input,expect", 
+    [
+        (create_depth(depth) + dir, TreeData(depth, True, dir.strip('/\\'), ''))
+        for dir in dir_variants
+        for depth in range(0, 4)
+    ]
+)
+def test_process_line_dir_returns_data(test_input, expect):
+    assert _process_line(test_input) == expect
+
+
+@pytest.mark.parametrize(
+    "test_input", 
+    [
+        (create_depth(depth) + ' ' + dir)
+        for dir in dir_variants
+        for depth in range(0, 4)
+    ]
+)
+def test_process_line_dir_odd_spaces_raises_exit(test_input):
+    try:
+        _process_line(test_input)
+        assert False
+    except ValueError as e:
+        assert True
+
+
+#todo: More Process Line Coverage
+#todo: Test Read Input Tree
+
