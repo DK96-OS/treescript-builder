fd760299862328c5d3c0b4cbbc0606ef49e4b96d
DK96-OS <69859316+DK96-OS@users.noreply.github.com>
2024-03-04 07:55:48 -0500

    Test: *u test_line_reader.py - create parametrized tests for _calculate_depth method, update name of _process_line method, remove create_depth method (moved to init module)

--- test/input/test_line_reader.py
+++ test/input/test_line_reader.py
@@ -1,6 +1,33 @@
 import unittest
-from input.line_reader import _calculate_depth, process_line
+import pytest
+from input.line_reader import _calculate_depth, _process_line
 from input.tree_data import TreeData
+from test.input import create_depth
+
+
+@pytest.mark.parametrize(
+    "test_input,expect", 
+    [ 
+        (create_depth(depth) + 'file', depth)
+        for depth in range(0, 9)
+    ]
+)
+def test_calculate_depth_file_returns_true(test_input, expect):
+    assert _calculate_depth(test_input) == expect
+
+# Directory Variants: A tuple of all possible ways that a directory may be represented.
+dir_variants = ('/dir', 'dir/', '\\dir', 'dir\\')
+
+@pytest.mark.parametrize(
+    "test_input,expect", 
+    [ 
+        (create_depth(depth) + dir, depth)
+        for dir in dir_variants
+        for depth in range(0, 9)
+    ]
+)
+def test_calculate_depth_dir_returns_true(test_input, expect):
+    assert _calculate_depth(test_input) == expect
 
 
 class TestLineReader(unittest.TestCase):
@@ -12,23 +39,18 @@ class TestLineReader(unittest.TestCase):
         """
         self.dir_variants = ('/dir', 'dir/', '\\dir', 'dir\\')
 
-    def test_create_depth_space(self):
-        for depth in range(0, 15):
-            str_len = len(create_depth(depth, 0))
-            self.assertEqual(str_len, 2 * depth)
-
     def test_calculate_depth_file(self):
         for depth in range(0, 15):
-            self.assertEqual(_calculate_depth(create_depth(depth, 0) + 'file'), depth)
+            self.assertEqual(_calculate_depth(create_depth(depth) + 'file'), depth)
 
     def test_create_depth_space_char_3(self):
         for depth in range(0, 15):
-            self.assertEqual(_calculate_depth(create_depth(depth, 3) + 'file'), depth)
+            self.assertEqual(_calculate_depth(create_depth(depth) + 'file'), depth)
 
     def test_calculate_depth_dir_variants(self):
         for var in self.dir_variants:
             for depth in range(0, 15):
-                input_line = create_depth(depth, 3) + var
+                input_line = create_depth(depth) + var
                 self.assertEqual(
                     _calculate_depth(input_line), depth
                 )
@@ -36,19 +58,19 @@ class TestLineReader(unittest.TestCase):
     def test_process_line_file_(self):
         file_name = 'file'
         for depth in range(3):
-            input_line = create_depth(depth, 3) + file_name
+            input_line = create_depth(depth) + file_name
             self.assertEqual(
-                process_line(input_line),
+                _process_line(input_line),
                 TreeData(depth, False, file_name, "")
             )
     
     def test_process_line_dir_(self):
         for dir_path in self.dir_variants:
             for depth in range(15):
-                input_line = create_depth(depth, 3) + dir_path
+                input_line = create_depth(depth) + dir_path
                 dir_name = dir_path.strip('/\\')
                 self.assertEqual(
-                    process_line(input_line),
+                    _process_line(input_line),
                     TreeData(depth, True, dir_name, "")
                 )
 
@@ -80,22 +102,3 @@ class TestLineReader(unittest.TestCase):
 
 if __name__ == '__main__':
     unittest.main()
-
-SPACE_CHARS = (' ', ' ', ' ', ' ')
-
-
-def create_depth(depth: int, space_char: int = 0) -> str:
-    """Creates a string of space chars equivalent to the given depth.
-
-	Parameters:
-	- depth (int): The amount of depth in the Tree Node Structure.
-	- space_char (int): The specific whitespace character to use. Default is the first.
-
-	Returns:
-	str: The Strings for each Space Char, of a given depth length.
-	"""
-    char = SPACE_CHARS[0]
-    if 0 < space_char < len(SPACE_CHARS):
-        char = SPACE_CHARS[space_char]
-    # Default
-    return char * depth * 2
