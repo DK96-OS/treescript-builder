1b3097ce5d20053d0d5bfa33ecb5342ce36fa574
DK96-OS <69859316+DK96-OS@users.noreply.github.com>
2024-03-04 07:46:49 -0500

    Main: *u __main__.py - simplify by deferring all program logic to the packages

--- ftb/__main__.py
+++ ftb/__main__.py
@@ -7,27 +7,11 @@ def main():
     Validates Arguments, then runs File Tree Builder.
     """
     import sys
-    from input import validate_input_arguments, read_input_instructions
-    from tree.tree_worker import TreeWorker
-    # Validate CL Arguments
-    input_data = validate_input_arguments(sys.argv)
-    # Use this Worker to execute Stateful Procedural Operations
-    worker = TreeWorker(input_data.data_dir)
-    # Is Builder or Reverse
-    if input_data.is_reversed:
-        for data in read_input_instructions(input_data.tree_input):
-            if not worker.remove(data):
-                raise Exception(
-                    "Invalid Operation: name=" + data.name + ", depth=" + data.depth
-                )
-        worker.cleanup_path_stack()
-    else:
-        for data in read_input_instructions(input_data.tree_input):
-            if not worker.build(data):
-                raise Exception(
-                    "Invalid Operation: name=" + data.name + ", depth=" + data.depth
-                )
-
+    from input import parse_arguments
+    from tree import build_tree
+    # Validate and Process Input
+    build_tree(parse_arguments(sys.argv))
+    
 
 if __name__ == "__main__":
     main()
