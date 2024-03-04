e8500b1c48137479382112ff85107f73369f24f0
DK96-OS <69859316+DK96-OS@users.noreply.github.com>
2024-03-04 07:50:03 -0500

    Tree: *u __init__.py - create method build_tree, optimize imports

--- tree/__init__.py
+++ tree/__init__.py
@@ -1,14 +1,29 @@
 """The Tree Module.
 """
-from input.tree_data import TreeData
-from tree.path_stack import PathStack
+from input import InputData
+from tree.tree_worker import TreeWorker
 
 
-def validate_tree_data(tree_data: tuple[TreeData]):
-    """
-    """
-    stack = PathStack()
-    #todo: Run through the tree data
+def build_tree(input_data: InputData):
+	"""
+	Build The Tree as defined by the InputData.
 
-    #todo: Compress Directory Operations
-
+	Parameters:
+	- input_data (str): The InputData produced by the Input Module.
+	"""
+    # Use this Worker to execute Stateful Procedural Operations
+    worker = TreeWorker(input_data.data_dir)
+    # Is Builder or Reverse
+    if input_data.is_reversed:
+        for data in input_data.get_tree_data():
+            if not worker.remove(data):
+                raise Exception(
+                    "Invalid Operation: name=" + data.name + ", depth=" + data.depth
+                )
+        worker.cleanup_path_stack()
+    else:
+        for data in input_data.get_tree_data():
+            if not worker.build(data):
+                raise Exception(
+                    "Invalid Operation: name=" + data.name + ", depth=" + data.depth
+                )
