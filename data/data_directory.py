a38aaef6591e54317b573e7f65fcf9ec12e96684
DK96-OS <69859316+DK96-OS@users.noreply.github.com>
2024-03-04 19:33:54 -0500

    Data: *u data_directory.py - debug init method, modify method search_label, remove method search_name

--- data/data_directory.py
+++ data/data_directory.py
@@ -1,29 +1,40 @@
 """Data Directory Management.
 """
-from typing import Optional
 from pathlib import Path
+from sys import exit
+from typing import Optional
+
+from data.data_files import is_valid_data_label
 
 
 class DataDirectory:
-    """Manages Access to the Data Directory.
+    """
+    Manages Access to the Data Directory.
+        Search for a Data Label, and obtain the Path to the Data File.
     """
 
-    def __init__(self, data_dir: Path, type_splitting: bool = False):
-        """
-        """
+    def __init__(self, data_dir: Path):
+        if not isinstance(data_dir, Path):
+            exit('The Data Directory must be a Path.')
+        if not data_dir.exists():
+            exit('The given Data Directory does not Exist.')
         self._data_dir = data_dir
-        self.type_splitting = type_splitting
 
-    def search_label(self, file_name: str, data_label: str) -> Optional[Path]:
+    def search_label(self, data_label: str) -> Optional[Path]:
         """
+        Search for a Data Label in this Data Directory.
+
+        Parameters:
+        - data_label (str): The Data Label to search for.
+
+        Returns:
+        Path (optional) - The Path to the Data File, or None.
         """
-        # todo:
-        self._data_dir.resolve(file_name)
+        if not is_valid_data_label(data_label):
+            return None
         #
-        pass
-
-    def search_name(self, file_name: str) -> Optional[Path]:
-        """
-        """
-        # todo:
-        pass
+        data_path = self._data_dir.resolve(data_label)
+        if not data_path.exists():
+            return None
+        #
+        return data_path
