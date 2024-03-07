3141e76ae2234c296cbffd064be9fab51b3fd865
DK96-OS <69859316+DK96-OS@users.noreply.github.com>
2024-03-07 15:46:15 -0500

    Test: *c test_tree_builder.py *c test_tree_trimmer.py

---
+++ test/tree/test_tree_builder.py
@@ -0,0 +1,10 @@
+"""Testing Tree Builder Methods.
+"""
+
+
+def test_create_file():
+	pass
+
+
+def test_make_dir_exist():
+	pass
---
+++ test/tree/test_tree_trimmer.py
@@ -0,0 +1,10 @@
+"""Testing Tree Trimming Methods.
+"""
+
+
+def test_remove_file():
+	pass
+
+
+def test_remove_dir():
+	pass
