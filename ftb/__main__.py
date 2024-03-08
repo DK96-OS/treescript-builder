acc2f07188d9bea44bb0b72bfbcdac3258441081
DK96-OS <69859316+DK96-OS@users.noreply.github.com>
2024-03-08 10:42:41 -0500

    Main: *u __main__.py - adjust the runtime system path to always include the project root directory

--- ftb/__main__.py
+++ ftb/__main__.py
@@ -14,4 +14,10 @@ def main():
     
 
 if __name__ == "__main__":
+    from pathlib import Path
+    # Get the directory of the current file (__file__ is the path to the script being executed)
+    current_directory = Path(__file__).resolve().parent.parent
+    # Add the directory to sys.path
+    sys.path.append(str(current_directory))
+    #
     main()
