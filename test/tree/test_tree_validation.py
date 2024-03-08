4c069179274cb89c1bd2a818d45f4e33e95734dc
DK96-OS <69859316+DK96-OS@users.noreply.github.com>
2024-03-08 15:43:53 -0500

    Test: *u test_tree_validation.py - add method generate_complex_tree, add test case for complex tree, update names of test cases

--- test/tree/test_tree_validation.py
+++ test/tree/test_tree_validation.py
@@ -1,5 +1,6 @@
 """Testing Tree Validation Methods.
 """
+from input.tree_data import TreeData
 import pytest
 from pathlib import Path
     
@@ -13,6 +14,29 @@ def generate_simple_tree():
     Simple Tree: a Directory and a File in that Directory.
     """
     return read_input_tree("src/\n  data.txt")
+
+def generate_complex_tree():
+    """
+    Complex Tree: an example Gradle-Java project.
+    """
+    yield TreeData(1, 0, True, '.github', '')
+    yield TreeData(2, 1, True, 'workflows', '')
+    yield TreeData(3, 0, True, 'module1', '')
+    yield TreeData(4, 1, False, 'build.gradle', '')
+    yield TreeData(5, 1, True, 'src', '')
+    yield TreeData(6, 2, True, 'main', '')
+    yield TreeData(7, 3, True, 'java', '')
+    yield TreeData(8, 4, True, 'com', '')
+    yield TreeData(9, 5, True, 'example', '')
+    yield TreeData(10, 6, False, 'Main.java', '')
+    yield TreeData(11, 2, True, 'test', '')
+    yield TreeData(12, 3, True, 'java', '')
+    yield TreeData(13, 4, True, 'com', '')
+    yield TreeData(14, 5, True, 'example', '')
+    yield TreeData(15, 6, False, 'MainTest.java', '')
+    yield TreeData(16, 0, False, 'README.md', '')
+    yield TreeData(17, 0, False, 'build.gradle', '')
+    yield TreeData(18, 0, False, 'settings.gradle', '')
 
 
 def generate_invalid_tree_line_1():
@@ -29,10 +53,25 @@ def generate_invalid_tree_line_2():
     return read_input_tree("src/\n    data.txt")
 
 
-def test_validate_tree_simple_tree():
+def test_validate_tree_simple_tree_returns_data():
     assert validate_tree(generate_simple_tree()) == (
         InstructionData(True, Path('src/'), None),
         InstructionData(False, Path('src/data.txt'), None),
+    )
+
+
+def test_validate_tree_complex_tree_returns_data():
+    assert validate_tree(generate_simple_tree()) == (
+        InstructionData(True, Path('.github/workflows/'), None),
+        InstructionData(True, Path('module1/'), None),
+        InstructionData(False, Path('module1/build.gradle'), None),
+        InstructionData(True, Path('module1/src/main/java/com/example/'), None),
+        InstructionData(False, Path('module1/src/main/java/com/example/Main.java'), None),
+        InstructionData(True, Path('module1/src/test/java/com/example/'), None),
+        InstructionData(False, Path('module1/src/test/java/com/example/MainTest.java'), None),
+        InstructionData(False, Path('README.md'), None),
+        InstructionData(False, Path('build.gradle'), None),
+        InstructionData(False, Path('settings.gradle'), None),
     )
 
 
@@ -43,9 +82,7 @@ def test_validate_tree_simple_tree():
         generate_invalid_tree_line_2(),
     ]
 )
-def test_validate_tree_invalid_tree(generator):
-    """
-    """
+def test_validate_tree_invalid_tree_raises_exit(generator):
     try:
         validate_tree(generator)
         assert False
@@ -53,7 +90,7 @@ def test_validate_tree_invalid_tree(generator):
         assert True
 
 
-def test_validate_with_data_dir_simple_tree():
+def test_validate_with_data_dir_simple_tree_returns_data():
     data_dir = pytest.MonkeyPatch()
     assert validate_with_data_dir(generate_simple_tree(), data_dir) == (
         InstructionData(True, Path('src/'), None),
@@ -68,9 +105,7 @@ def test_validate_with_data_dir_simple_tree():
         generate_invalid_tree_line_2(),
     ]
 )
-def test_validate_with_data_dir_invalid_tree(generator):
-    """
-    """
+def test_validate_with_data_dir_invalid_tree_raises_exit(generator):
     data_dir = pytest.MonkeyPatch()
     try:
         validate_with_data_dir(generator, data_dir)
