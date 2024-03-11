22dd3a0835b5e4d5df2b4f5256d9e76d6e90febe
DK96-OS <69859316+DK96-OS@users.noreply.github.com>
2024-03-11 10:46:30 -0400

    Tree: *u tree_state.py - add method validate_tree_data, modify method process_queue to return Path, add method _update_line_number

--- tree/tree_state.py
+++ tree/tree_state.py
@@ -1,8 +1,11 @@
 """Tree State.
     A Key component in Tree Validation for Build operations.
 """
+from pathlib import Path
+from sys import exit
 from typing import Optional
 
+from input.tree_data import TreeData
 from tree.path_stack import PathStack
 
 
@@ -14,6 +17,27 @@ class TreeState:
     def __init__(self):
         self._stack = PathStack()
         self._queue = []
+        self._prev_line_number = 0
+
+    def validate_tree_data(self, tree_data: TreeData) -> int:
+        """
+        Ensure that the next TreeData is valid, relative to current state.
+            Calculate the change in depth, occurring with this TreeData.
+
+        Parameters:
+        - tree_data (TreeData): The next TreeData in the sequence to Validate.
+
+        Returns:
+        int - The difference between the TreeData depth and the TreeState depth.
+
+        Raises:
+        SystemExit - When the TreeData is invalid, relative to the current TreeState.
+        """
+        self._update_line_number(tree_data.line_number)
+        # Calculate the Change in Depth
+        if tree_data.depth < 0:
+            exit("Invalid Depth Value")
+        return tree_data.depth - self.get_current_depth()
 
     def get_current_depth(self) -> int:
         """
@@ -54,13 +78,13 @@ class TreeState:
         """
         self._stack.push(dir_name)
 
-    def process_queue(self) -> Optional[str]:
+    def process_queue(self) -> Optional[Path]:
         """
         Process the Directories in the Queue.
             Adds all directories from the Queue to the Stack.
 
         Returns:
-        str (optional) - The whole directory from the Stack.
+        Path (optional) - The whole Path from the Stack.
         """
         if len(self._queue) < 1:
             return None
@@ -81,3 +105,18 @@ class TreeState:
         str - The directory that was popped from the Stack.
         """
         return self._stack.reduce_depth(depth)
+
+    def _update_line_number(self, line_number: int):
+        """
+        Validate the Line Number is always increasing.
+
+        Parameters:
+        - line_number (int): The line number from the next TreeData.
+
+        Raises:
+        SystemExit - When the Line Number does not increase.
+        """
+        if self._prev_line_number < line_number:
+            self._prev_line_number = line_number
+        else:
+            exit("Invalid Tree Data Sequence")
