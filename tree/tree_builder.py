ea67e23caf90b80d03d67ba9bc3e45e66a27f976
DK96-OS <69859316+DK96-OS@users.noreply.github.com>
2024-03-10 20:34:59 -0400

    Tree: *u tree_builder.py - add method validate_build, add method validate_build_with_data

--- tree/tree_builder.py
+++ tree/tree_builder.py
@@ -1,8 +1,78 @@
 """Tree Building Operations
 """
 from pathlib import Path
+from typing import Generator
 
+from input.data_directory import DataDirectory
 from tree.instruction_data import InstructionData
+from input.tree_data import TreeData
+from tree.tree_state import TreeState
+
+
+def validate_build(
+    tree_data: Generator[TreeData, None, None]
+    ) -> Generator[InstructionData, None, None]:
+    """
+    Validate the Build Instructions.
+
+    Parameters:
+    - tree_data (Generator[TreeData]): The Generator that provides TreeData.
+
+    Returns:
+    Generator[TreeData] - A generator that yields Instructions.
+    """
+    tree_state = TreeState()
+    previous_line_number = 0
+    #
+    for node in tree_data:
+        # First, check the Line Number
+        if previous_line_number < node.line_number:
+            previous_line_number = node.line_number
+        else:
+            exit("Invalid Line Number")
+        # Next, check the Tree Depth
+        depth_change = node.depth - tree_state.get_current_depth()
+        #
+        if depth_change > 1:
+            exit(f"Invalid Depth at Line: {node.line_number}")
+        elif depth_change == 1:
+            if node.is_dir:
+                # Enqueue New Directory
+                tree_state.add_to_queue(node.name)
+            else:
+                # Merge Queue into Stack
+                new_dir = tree_state.process_queue()
+                if new_dir is not None:
+                    # Build Directory
+                    yield InstructionData(True, new_dir, None)
+                # Build File
+                yield InstructionData(False, new_dir + node.name, None)
+        else:
+            # Merge Queue into Stack
+            new_dir = tree_state.process_queue()
+            if new_dir is not None:
+                yield InstructionData(True, new_dir, None)
+            # Pop Stack to required Depth
+            if not tree_state.reduce_depth(node.depth):
+                exit(f"Invalid Depth at Line: {node.line_number}")
+            if node.is_dir:
+                tree_state.add_to_queue(node.name)
+            else:
+                yield InstructionData(False, tree_state.get_current_path() + node.name, None)
+    # Always Finish Build Sequence with ProcessQueue
+    remaining_dirs = tree_state.process_queue()
+    if remaining_dirs is not None:
+        yield InstructionData(True, Path(remaining_dirs), None)
+
+
+def validate_build_with_data(
+    tree_data: Generator[TreeData],
+    data_dir: DataDirectory
+    ) -> tuple[InstructionData]:
+    """
+    Validate the Build Instructions with a Data Directory.
+    """
+    pass
 
 
 def build(instructions: tuple[InstructionData]):
