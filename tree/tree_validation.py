1f4d3c8233909c84f08f41e57b9a678d4a854bac
DK96-OS <69859316+DK96-OS@users.noreply.github.com>
2024-03-11 14:10:20 -0400

    Tree: *u tree_validation.py - add method validate_build, add method validate_trim, remove method validate_tree, add internal generator methods for build and trim

--- tree/tree_validation.py
+++ tree/tree_validation.py
@@ -1,64 +1,196 @@
 """Tree Validation Methods.
 """
-from itertools import takewhile
 from pathlib import Path
-from typing import Generator
+from typing import Generator, Optional
 
 from input.data_directory import DataDirectory
 from input.tree_data import TreeData
 from tree.instruction_data import InstructionData
 from tree.path_stack import PathStack
+from tree.tree_state import TreeState
 
 
-def validate_tree(
-    tree_data_generator: Generator[TreeData, None, None]
-    ) -> tuple[InstructionData, ...]:
+def validate_build(
+    tree_data: Generator[TreeData, None, None],
+    data_dir: Optional[DataDirectory]
+) -> tuple[InstructionData, ...]:
     """
-    Process Tree Data into executable instructions.
-        Assume there is no data directory.
+    Validate the Build Instructions.
+
+    Parameters:
+    - tree_data (Generator[TreeData]): The Generator that provides TreeData.
+    - data_dir (DataDirectory, optional): The optional Data Directory.
+
+    Returns:
+    tuple[InstructionData] - A generator that yields Instructions.
     """
-    tree_Data = _validate_tree_structure(tree_data_generator)
-    #
-    inst = []
-    path_stack = PathStack()
-    # More state
-    last_tree_node = None
-    #
-    for t_data in tree_data_generator:
-        # Check Is Directory
-        if t_data.is_dir:
-            #
-            success = path_stack.reduce_depth(t_data.depth)
-            if not success:
-                exit(f"Inconsistent Tree Depth at Line: {t_data.line_number}")
-            #
-            path_stack.push(t_data.name)
-            # todo: Optimize 
-            inst.append(InstructionData(True, Path(path_stack.join_stack()), None))
+    if data_dir is None:
+        return tuple(_validate_build_generator(tree_data))
+    else:
+        return tuple(_validate_build_generator(tree_data, data_dir))
+
+
+def validate_trim(
+    tree_data: Generator[TreeData, None, None],
+    data_dir: Optional[DataDirectory]
+) -> tuple[InstructionData]:
+    """    
+    Validate the Trim Instructions.
+
+    Parameters:
+    - tree_data (Generator[TreeData]): The Generator that provides TreeData.
+    - data_dir (DataDirectory, optional): The optional Data Directory.
+
+    Returns:
+    tuple[InstructionData] - A generator that yields Instructions.
+    """
+    if data_dir is None:
+        return tuple(_validate_trim_generator(tree_data))
+    else:
+        return tuple(_validate_trim_generator(tree_data, data_dir))
+
+
+def _validate_build_generator(
+    tree_data: Generator[TreeData, None, None]
+) -> Generator[TreeData, None, None]:
+    tree_state = TreeState()
+    for node in tree_data:
+        # Error if any Nodes have Data Labels
+        if node.data_label != '':
+            exit(f"Found Data Label on Line {node.line_number} with no Data Directory: {node.data_label}")
+        # Calculate Tree Depth Change
+        if tree_state.validate_tree_data(node) == 1:
+            if node.is_dir:
+                tree_state.add_to_queue(node.name)
+            else:
+                # Merge Queue into Stack
+                new_dir = tree_state.process_queue()
+                if new_dir is not None:
+                    yield InstructionData(True, new_dir, None)
+                yield InstructionData(False, new_dir / node.name, None)
         else:
-            file_path = path_stack.create_path(t_data.name)
-            inst.append(InstructionData(False, file_path, None))
-        #
-    # todo: Empty Path Stack
-    return tuple(inst)
+            # Merge Queue into Stack
+            new_dir = tree_state.process_queue()
+            if new_dir is not None:
+                yield InstructionData(True, new_dir, None)
+            # Pop Stack to required Depth
+            if not tree_state.reduce_depth(node.depth):
+                exit(f"Invalid Depth at Line: {node.line_number}")
+            if node.is_dir:
+                tree_state.add_to_queue(node.name)
+            else:
+                yield InstructionData(False, tree_state.get_current_path() + node.name, None)
+    # Always Finish Build Sequence with ProcessQueue
+    remaining_dirs = tree_state.process_queue()
+    if remaining_dirs is not None:
+        yield InstructionData(True, remaining_dirs, None)
 
 
-def validate_with_data_dir(
-    tree_data_generator: Generator[TreeData, None, None],
+def _validate_build_generator(
+    tree_data: Generator[TreeData, None, None],
     data_dir: DataDirectory
-    ) -> tuple[InstructionData, ...]:
-    """
-    Validate Tree alongside the Data directory.
-    """
-    inst = []
-    path_stack = PathStack()
-    return ()
+) -> Generator[TreeData, None, None]:
+    tree_state = TreeState()
+    for node in tree_data:
+        # Calculate Tree Depth Change
+        if tree_state.validate_tree_data(node) == 1:
+            if node.is_dir:
+                tree_state.add_to_queue(node.name)
+            else:
+                # Build Queued Directories
+                new_dir = tree_state.process_queue()
+                if new_dir is not None:
+                    yield InstructionData(True, new_dir, None)
+                # Build File
+                yield InstructionData(
+                    False,
+                    new_dir / node.name,
+                    data_dir.process_tree_data(node)
+                )
+        else:
+            # Merge Queue into Stack
+            new_dir = tree_state.process_queue()
+            if new_dir is not None:
+                yield InstructionData(True, new_dir, None)
+            # Pop Stack to required Depth
+            if not tree_state.reduce_depth(node.depth):
+                exit(f"Invalid Tree Depth on Line {node.line_number} : {node.name}")
+            if node.is_dir:
+                tree_state.add_to_queue(node.name)
+            else:
+                yield InstructionData(
+                    False,
+                    tree_state.get_current_path() / node.name,
+                    data_dir.process_tree_data(node)
+                )
+    # Always Finish Build Sequence with ProcessQueue
+    remaining_dirs = tree_state.process_queue()
+    if remaining_dirs is not None:
+        yield InstructionData(True, remaining_dirs, None)
 
 
-def _validate_tree_structure(tree_generator: Generator[TreeData, None, None]) -> tuple[TreeData]:
-    data = []
-    for t in tree_generator:
-        data.append(t)
-    #
-    pass
-    return tuple(data)
+def _validate_trim_generator(
+    tree_data: Generator[TreeData, None, None]
+) -> Generator[TreeData, None, None]:
+    tree_state = TreeState()
+    for node in tree_data:
+        # Calculate Tree Depth Change
+        if tree_state.validate_tree_data() == 1:
+            if node.is_dir:
+                tree_state.add_to_stack(node.name)
+            else:
+                yield InstructionData(
+                    False,
+                    tree_state.get_current_path() / node.name,
+                    None
+                )
+        else:
+            # Pop Stack to required Depth
+            for i in tree_state.process_stack(node.depth):
+                yield InstructionData(True, i, None)
+            # Dir or File
+            if node.is_dir:
+                tree_state.add_to_stack(node.name)
+            else:
+                yield InstructionData(
+                    False,
+                    tree_state.get_current_path() / node.name,
+                    None
+                )
+    # Finish Trim Sequence with Pop Stack
+    for i in tree_state.process_stack(0):
+        yield InstructionData(True, i, None)
+
+
+def _validate_trim_generator(
+    tree_data: Generator[TreeData, None, None],
+    data_dir: DataDirectory
+) -> Generator[TreeData, None, None]:
+    tree_state = TreeState()
+    for node in tree_data:
+        # Calculate Tree Depth Change
+        if tree_state.validate_tree_data() == 1:
+            if node.is_dir:
+                tree_state.add_to_stack(node.name)
+            else:
+                yield InstructionData(
+                    False,
+                    tree_state.get_current_path() / node.name,
+                    data_dir.check_trim(node)
+                )
+        else:
+            # Pop Stack to required Depth
+            for i in tree_state.process_stack(node.depth):
+                yield InstructionData(True, i, None)
+            # Dir or File
+            if node.is_dir:
+                tree_state.add_to_stack(node.name)
+            else:
+                yield InstructionData(
+                    False,
+                    tree_state.get_current_path() / node.name,
+                    data_dir.check_trim(node)
+                )
+    # Finish Trim Sequence with Pop Stack
+    for i in tree_state.process_stack(0):
+        yield InstructionData(True, i, None)
