61b25b7199640df7f19a2fc7f4a13fd5f9cc62c4
DK96-OS <69859316+DK96-OS@users.noreply.github.com>
2024-03-16 12:02:43 -0400

    Test: *r test_tree_validation.py - divide into tree_generators and operation specific test suites *c tree_generators.py *c test_build_validation.py

--- test/tree/test_build_validation.py
+++ test/tree/test_build_validation.py
@@ -1,98 +1,16 @@
-"""Testing Tree Validation Methods.
+"""Testing Build Validation Methods.
 """
 import pytest
 from pathlib import Path
 
 from input.data_directory import DataDirectory
-from input.tree_data import TreeData
-from input.line_reader import read_input_tree
 from tree.instruction_data import InstructionData
-from tree.tree_validation import validate_build, validate_trim
+from tree.build_validation import validate_build
 
-
-def generate_simple_tree():
-    """
-    Simple Tree: a Directory and a File in that Directory.
-    """
-    return read_input_tree("src/\n  data.txt")
-
-
-def generate_simple_tree2():
-    """
-    Simple Tree: a Directory and a File in that Directory.
-    """
-    yield TreeData(1, 0, True, 'src', '')
-    yield TreeData(2, 1, False, 'data.txt', '')
-
-
-def generate_gradle_module_tree():
-    """
-    Tree Template: a Gradle Module for Java.
-    """
-    yield TreeData(1, 0, True, 'module1', '')
-    yield TreeData(2, 1, False, 'build.gradle', '')
-    yield TreeData(3, 1, True, 'src', '')
-    yield TreeData(4, 2, True, 'main', '')
-    yield TreeData(5, 3, True, 'java', '')
-    yield TreeData(6, 2, True, 'test', '')
-    yield TreeData(7, 3, True, 'java', '')
-
-
-def generate_python_package_tree():
-    """
-    Tree Template: a Python Package
-    """
-    yield TreeData(1, 0, True, 'package_name', '')
-    yield TreeData(2, 1, False, '__init__.py', '')
-    yield TreeData(3, 1, False, 'internal_module.py', '')
-
-
-def generate_complex_tree():
-    """
-    Complex Tree: an example Gradle-Java project.
-    """
-    yield TreeData(1, 0, True, '.github', '')
-    yield TreeData(2, 1, True, 'workflows', '')
-    yield TreeData(3, 0, True, 'module1', '')
-    yield TreeData(4, 1, False, 'build.gradle', '')
-    yield TreeData(5, 1, True, 'src', '')
-    yield TreeData(6, 2, True, 'main', '')
-    yield TreeData(7, 3, True, 'java', '')
-    yield TreeData(8, 4, True, 'com', '')
-    yield TreeData(9, 5, True, 'example', '')
-    yield TreeData(10, 6, False, 'Main.java', '')
-    yield TreeData(11, 2, True, 'test', '')
-    yield TreeData(12, 3, True, 'java', '')
-    yield TreeData(13, 4, True, 'com', '')
-    yield TreeData(14, 5, True, 'example', '')
-    yield TreeData(15, 6, False, 'MainTest.java', '')
-    yield TreeData(16, 0, False, 'README.md', '')
-    yield TreeData(17, 0, False, 'build.gradle', '')
-    yield TreeData(18, 0, False, 'settings.gradle', '')
-
-
-def generate_invalid_tree_line_1():
-    """
-    The First TreeData generated has a depth that is inconsistent with tree state.
-    """
-    return read_input_tree("  src/\n")
-
-
-def generate_invalid_tree_line_2():
-    """
-    The Second TreeData generated has a depth that is inconsistent with tree state.
-    """
-    return read_input_tree("src/\n    data.txt")
+from test.tree.tree_generators import *
 
 
 def test_validate_build_simple_tree_returns_data():
-    assert validate_build(generate_simple_tree(), None) == (
-        InstructionData(True, Path('src/'), None),
-        InstructionData(False, Path('src/data.txt'), None),
-    )
-
-
-def test_validate_build_simple_tree2_returns_data():
     assert validate_build(generate_simple_tree(), None) == (
         InstructionData(True, Path('src/'), None),
         InstructionData(False, Path('src/data.txt'), None),
@@ -150,19 +68,8 @@ def test_validate_build_with_data_dir_simple_tree_returns_data():
     with pytest.MonkeyPatch().context() as c:
         c.setattr(Path, 'exists', lambda _: True)
         data_dir = DataDirectory(Path('.ftb/data/'))
-        
+        #
         assert validate_build(generate_simple_tree(), data_dir) == (
-            InstructionData(True, Path('src/'), None),
-            InstructionData(False, Path('src/data.txt'), None),
-        )
-
-
-def test_validate_build_with_data_dir_simple_tree2_returns_data():
-    with pytest.MonkeyPatch().context() as c:
-        c.setattr(Path, 'exists', lambda _: True)
-        data_dir = DataDirectory(Path('.ftb/data/'))
-        
-        assert (generate_simple_tree2(), data_dir) == (
             InstructionData(True, Path('src/'), None),
             InstructionData(False, Path('src/data.txt'), None),
         )
---
+++ test/tree/tree_generators.py
@@ -0,0 +1,73 @@
+"""Tree Generators used by Test Suites.
+"""
+from input.tree_data import TreeData
+
+
+def generate_simple_tree():
+    """
+    Simple Tree: a Directory and a File in that Directory.
+    """
+    yield TreeData(1, 0, True, 'src', '')
+    yield TreeData(2, 1, False, 'data.txt', '')
+
+
+def generate_gradle_module_tree():
+    """
+    Tree Template: a Gradle Module for Java.
+    """
+    yield TreeData(1, 0, True, 'module1', '')
+    yield TreeData(2, 1, False, 'build.gradle', '')
+    yield TreeData(3, 1, True, 'src', '')
+    yield TreeData(4, 2, True, 'main', '')
+    yield TreeData(5, 3, True, 'java', '')
+    yield TreeData(6, 2, True, 'test', '')
+    yield TreeData(7, 3, True, 'java', '')
+
+
+def generate_python_package_tree():
+    """
+    Tree Template: a Python Package
+    """
+    yield TreeData(1, 0, True, 'package_name', '')
+    yield TreeData(2, 1, False, '__init__.py', '')
+    yield TreeData(3, 1, False, 'internal_module.py', '')
+
+
+def generate_complex_tree():
+    """
+    Complex Tree: an example Gradle-Java project.
+    """
+    yield TreeData(1, 0, True, '.github', '')
+    yield TreeData(2, 1, True, 'workflows', '')
+    yield TreeData(3, 0, True, 'module1', '')
+    yield TreeData(4, 1, False, 'build.gradle', '')
+    yield TreeData(5, 1, True, 'src', '')
+    yield TreeData(6, 2, True, 'main', '')
+    yield TreeData(7, 3, True, 'java', '')
+    yield TreeData(8, 4, True, 'com', '')
+    yield TreeData(9, 5, True, 'example', '')
+    yield TreeData(10, 6, False, 'Main.java', '')
+    yield TreeData(11, 2, True, 'test', '')
+    yield TreeData(12, 3, True, 'java', '')
+    yield TreeData(13, 4, True, 'com', '')
+    yield TreeData(14, 5, True, 'example', '')
+    yield TreeData(15, 6, False, 'MainTest.java', '')
+    yield TreeData(16, 0, False, 'README.md', '')
+    yield TreeData(17, 0, False, 'build.gradle', '')
+    yield TreeData(18, 0, False, 'settings.gradle', '')
+
+
+def generate_invalid_tree_line_1():
+    """
+    The First TreeData generated has a depth that is inconsistent with tree state.
+    """
+    from input.line_reader import read_input_tree
+    return read_input_tree("  src/\n")
+
+
+def generate_invalid_tree_line_2():
+    """
+    The Second TreeData generated has a depth that is inconsistent with tree state.
+    """
+    from input.line_reader import read_input_tree
+    return read_input_tree("src/\n    data.txt")
