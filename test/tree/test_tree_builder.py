7dac1f498ba022051ed6a48107b82fbf7e814145
DK96-OS <69859316+DK96-OS@users.noreply.github.com>
2024-03-17 09:31:59 -0400

    Test: *u test_tree_builder.py *c test_trim_validation.py

--- test/tree/test_tree_builder.py
+++ test/tree/test_tree_builder.py
@@ -1,10 +1,50 @@
 """Testing Tree Builder Methods.
 """
+import pytest
+from pathlib import Path
+
+from tree.instruction_data import InstructionData
+from tree.tree_builder import build
 
 
-def test_create_file():
-	pass
+def test_build_one_directory_already_exists_returns_true():
+	instructions = (
+		InstructionData(True, Path('./src'), None),
+	)
+	with pytest.MonkeyPatch().context() as m:
+		m.setattr(Path, 'exists', lambda _: True)
+	assert build(instructions) == (True)
 
 
-def test_make_dir_exist():
-	pass
+def test_build_one_directory_does_not_exist_succeeds_returns_true():
+	instructions = (
+		InstructionData(True, Path('./src'), None),
+	)
+	with pytest.MonkeyPatch().context() as m:
+		m.setattr(Path, 'exists', lambda _: False) 
+		m.setattr(Path, 'mkdir', lambda _: False)
+		#
+	assert build(instructions) == (True)
+
+
+def test_build_one_directory_does_not_exist_fails_returns_false():
+	instructions = (
+		InstructionData(True, Path('./src'), None),
+	)
+	with pytest.MonkeyPatch().context() as m:
+		m.setattr(Path, 'exists', lambda _: False) 
+		def raise_error():
+			raise IOError()
+		m.setattr(Path, 'mkdir', raise_error)
+		#
+	assert build(instructions) == (False)
+
+
+def test_build_one_file():
+	instructions = (
+		InstructionData(True, Path('./src'), None),
+		InstructionData(False, Path('./src/data.txt'), None),
+	)
+	with pytest.MonkeyPatch().context() as m:
+		m.setattr(Path, 'exists', lambda _: True)
+	# todo: assert
---
+++ test/tree/test_trim_validation.py
@@ -0,0 +1,123 @@
+"""Testing Tree Validation Methods.
+"""
+import pytest
+from pathlib import Path
+
+from input.data_directory import DataDirectory
+from tree.instruction_data import InstructionData
+from tree.trim_validation import validate_trim
+
+from test.tree.tree_generators import *
+
+
+def test_validate_trim_simple_tree_returns_data():
+    assert validate_trim(generate_simple_tree(), None) == (
+        InstructionData(True, Path('src/'), None),
+        InstructionData(False, Path('src/data.txt'), None),
+    )
+
+
+def test_validate_trim_gradle_module_tree_returns_data():
+    assert validate_trim(generate_gradle_module_tree(), None) == (
+        InstructionData(True, Path('module1/'), None),
+        InstructionData(False, Path('module1/build.gradle'), None),
+        InstructionData(True, Path('module1/src/main/java/'), None),
+        InstructionData(True, Path('module1/src/test/java/'), None),
+    )
+
+
+def test_validate_trim_python_package_tree_returns_data():
+    assert validate_trim(generate_python_package_tree(), None) == (
+        InstructionData(True, Path('package_name/'), None),
+        InstructionData(False, Path('package_name/__init__.py'), None),
+        InstructionData(False, Path('package_name/internal_module.py'), None),
+    )
+
+
+def test_validate_trim_complex_tree_returns_data():
+    assert validate_trim(generate_complex_tree(), None) == (
+        InstructionData(True, Path('.github/workflows/'), None),
+        InstructionData(True, Path('module1/'), None),
+        InstructionData(False, Path('module1/build.gradle'), None),
+        InstructionData(True, Path('module1/src/main/java/com/example/'), None),
+        InstructionData(False, Path('module1/src/main/java/com/example/Main.java'), None),
+        InstructionData(True, Path('module1/src/test/java/com/example/'), None),
+        InstructionData(False, Path('module1/src/test/java/com/example/MainTest.java'), None),
+        InstructionData(False, Path('README.md'), None),
+        InstructionData(False, Path('build.gradle'), None),
+        InstructionData(False, Path('settings.gradle'), None),
+    )
+
+
+@pytest.mark.parametrize(
+    'generator',
+    [
+        generate_invalid_tree_line_1(),
+        generate_invalid_tree_line_2(),
+    ]
+)
+def test_validate_trim_invalid_tree_raises_exit(generator):
+    try:
+        validate_trim(generator, None)
+        assert False
+    except SystemExit as e:
+        assert True
+
+
+def test_validate_trim_with_data_dir_simple_tree_returns_data():
+    with pytest.MonkeyPatch().context() as c:
+        c.setattr(Path, 'exists', lambda _: True)
+        data_dir = DataDirectory(Path('.ftb/data/'))
+        #
+        assert validate_trim(generate_simple_tree(), data_dir) == (
+            InstructionData(True, Path('src/'), None),
+            InstructionData(False, Path('src/data.txt'), None),
+        )
+
+
+def test_validate_trim_with_data_dir_gradle_module_tree_returns_data():
+    with pytest.MonkeyPatch().context() as c:
+        c.setattr(Path, 'exists', lambda _: True)
+        data_dir = DataDirectory(Path('.ftb/data/'))
+        assert validate_trim(generate_gradle_module_tree(), data_dir) == (
+            InstructionData(True, Path('module1/'), None),
+            InstructionData(False, Path('module1/build.gradle'), Path('.ftb/data/gbuild_module1')),
+            InstructionData(True, Path('module1/src/main/java/'), None),
+            InstructionData(True, Path('module1/src/test/java/'), None),
+        )
+
+
+def test_validate_trim_with_data_dir_complex_tree_returns_data():
+    with pytest.MonkeyPatch().context() as c:
+        c.setattr(Path, 'exists', lambda _: True)
+        data_dir = DataDirectory(Path('.ftb/data/'))
+        assert validate_trim(generate_complex_tree(), data_dir) == (
+            InstructionData(True, Path('.github/workflows/'), None),
+            InstructionData(True, Path('module1/'), None),
+            InstructionData(False, Path('module1/build.gradle'), None),
+            InstructionData(True, Path('module1/src/main/java/com/example/'), None),
+            InstructionData(False, Path('module1/src/main/java/com/example/Main.java'), None),
+            InstructionData(True, Path('module1/src/test/java/com/example/'), None),
+            InstructionData(False, Path('module1/src/test/java/com/example/MainTest.java'), None),
+            InstructionData(False, Path('README.md'), None),
+            InstructionData(False, Path('build.gradle'), None),
+            InstructionData(False, Path('settings.gradle'), None),
+        )
+
+
+@pytest.mark.parametrize(
+    'generator',
+    [
+        generate_invalid_tree_line_1(),
+        generate_invalid_tree_line_2(),
+    ]
+)
+def test_validate_trim_with_data_dir_invalid_tree_raises_exit(generator):
+    with pytest.MonkeyPatch().context() as c:
+        c.setattr(Path, 'exists', lambda _: True)
+        data_dir = DataDirectory(Path('.ftb/data/'))
+        try:
+            validate_trim(generator, data_dir)
+            assert False
+        except SystemExit as e:
+            assert True
