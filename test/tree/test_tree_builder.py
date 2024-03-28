05fdc9cfdeb0696a64f17d8a8e9f379a25cd3772
DK96-OS <69859316+DK96-OS@users.noreply.github.com>
2024-03-28 12:59:38 -0400

    Test: *u test_tree_builder.py - add mocking, add test cases, improve test cases

--- test/tree/test_tree_builder.py
+++ test/tree/test_tree_builder.py
@@ -1,5 +1,6 @@
 """Testing Tree Builder Methods.
 """
+from unittest.mock import MagicMock
 import pytest
 from pathlib import Path
 
@@ -8,23 +9,22 @@ from tree.tree_builder import build
 
 
 def test_build_one_directory_already_exists_returns_true():
-	instructions = (
-		InstructionData(True, Path('./src'), None),
-	)
 	with pytest.MonkeyPatch().context() as m:
+		instructions = (
+			InstructionData(True, Path('./src'), None),
+		)
 		m.setattr(Path, 'exists', lambda _: True)
-	assert build(instructions) == (True)
+		assert build(instructions) == (True,)
 
 
 def test_build_one_directory_does_not_exist_succeeds_returns_true():
-	instructions = (
-		InstructionData(True, Path('./src'), None),
-	)
 	with pytest.MonkeyPatch().context() as m:
-		m.setattr(Path, 'exists', lambda _: False) 
-		m.setattr(Path, 'mkdir', lambda _: False)
-		#
-	assert build(instructions) == (True)
+		instructions = (
+			InstructionData(True, Path('./src'), None),
+		)
+		m.setattr(Path, 'exists', lambda _: False)
+		m.setattr(Path, 'mkdir', MagicMock())
+		assert build(instructions) == (True,)
 
 
 def test_build_one_directory_does_not_exist_fails_returns_false():
@@ -32,19 +32,27 @@ def test_build_one_directory_does_not_exist_fails_returns_false():
 		InstructionData(True, Path('./src'), None),
 	)
 	with pytest.MonkeyPatch().context() as m:
-		m.setattr(Path, 'exists', lambda _: False) 
+		m.setattr(Path, 'exists', lambda _: False)
 		def raise_error():
 			raise IOError()
-		m.setattr(Path, 'mkdir', raise_error)
-		#
-	assert build(instructions) == (False)
+		m.setattr(Path, 'mkdir', lambda *args, **kwargs: raise_error())
+		assert build(instructions) == (False,)
 
 
-def test_build_one_file():
-	instructions = (
-		InstructionData(True, Path('./src'), None),
-		InstructionData(False, Path('./src/data.txt'), None),
-	)
+def test_build_one_file_does_not_exist_succeeds_returns_true():
 	with pytest.MonkeyPatch().context() as m:
+		instructions = (
+			InstructionData(False, Path('./data.txt'), None),
+		)
+		m.setattr(Path, 'exists', lambda _: False)
+		m.setattr(Path, 'touch', lambda *args, **kwargs: None)
+		assert build(instructions) == (True,)
+
+
+def test_build_one_file_already_exists_touch_returns_true():
+	with pytest.MonkeyPatch().context() as m:
+		instructions = (
+			InstructionData(False, Path('./data.txt'), None),
+		)
 		m.setattr(Path, 'exists', lambda _: True)
-	# todo: assert
+		assert build(instructions) == (True,)
