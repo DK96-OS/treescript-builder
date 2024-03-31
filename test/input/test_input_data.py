45917d5771f099f86edcf11c3865e0e39ea4f362
DK96-OS <69859316+DK96-OS@users.noreply.github.com>
2024-03-31 11:43:05 -0400

    Test: *c test_input_data.py *u test_tree_trimmer.py - add test cases

---
+++ test/input/test_input_data.py
@@ -0,0 +1,16 @@
+"""Testing Input Data.
+"""
+from input.input_data import InputData
+from input.tree_data import TreeData
+
+
+def test_get_tree_data_single_dir_returns_data():
+    i = InputData('src/', None, False)
+    gen = i.get_tree_data()
+    assert list(gen) == [TreeData(1, 0, True, 'src', '')]
+
+
+def test_get_tree_data_file_in_dir_returns_data():
+    i = InputData('src/\n  data.txt', None, False)
+    gen = i.get_tree_data()
+    assert list(gen) == [TreeData(1, 0, True, 'src', ''), TreeData(2, 1, False, 'data.txt', '')]
--- test/tree/test_tree_trimmer.py
+++ test/tree/test_tree_trimmer.py
@@ -1,10 +1,26 @@
 """Testing Tree Trimming Methods.
 """
+from pathlib import Path
+import pytest
+
+from tree.instruction_data import InstructionData
+from tree.tree_trimmer import trim
 
 
-def test_remove_file():
-	pass
+def test_trim_src_dir_returns_true():
+	with pytest.MonkeyPatch().context() as c:
+		c.setattr(Path, 'rmdir', lambda _: True)
+		#
+		i = (InstructionData(True, Path('src/'), None), )
+		results = trim(i)
+		assert results[0] == True
+		assert len(results) == 1
 
 
-def test_remove_dir():
-	pass
+def test_trim_file_returns_true():
+	with pytest.MonkeyPatch().context() as c:
+		c.setattr(Path, 'unlink', lambda _, **kwargs: True)
+		i = (InstructionData(False, Path('data.txt'), None), )
+		results = trim(i)
+		assert results[0] == True
+		assert len(results) == 1
