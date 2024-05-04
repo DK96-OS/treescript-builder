7a4c2cd0ce6cb4d6e2d9ad875e1a427077a562d5
DK96-OS <69859316+DK96-OS@users.noreply.github.com>
2024-05-04 15:30:57 -0400

    Main: *m __main__.py to tree directory

---
+++ tree/__main__.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python
+from pathlib import Path
+import sys
+
+from input import validate_input_arguments
+from tree import build_tree
+
+
+def main():
+    """Validates Arguments, then runs File Tree Builder."""
+    build_tree(
+        validate_input_arguments(sys.argv[1:])
+    )
+
+
+if __name__ == "__main__":
+    # Get the directory of the current file (__file__ is the path to the script being executed)
+    current_directory = Path(__file__).resolve().parent.parent
+    # Add the directory to sys.path
+    sys.path.append(str(current_directory))
+    #
+    main()
