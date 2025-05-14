f3d2b2de5953896ab8fe1c7b4308daecfdf9ac5f
DK96-OS <69859316+dk96-os@users.noreply.github.com>
2025-05-14 12:10:37 -0400

    Test Input Package: * Update test_input_data.py - replace usage of InputData method (which is being removed) with an imported line_reader method

--- test/treescript_builder/input/test_input_data.py
+++ test/treescript_builder/input/test_input_data.py
@@ -2,15 +2,16 @@
 """
 from treescript_builder.data.tree_data import TreeData
 from treescript_builder.input.input_data import InputData
+from treescript_builder.input.line_reader import read_input_tree
 
 
 def test_get_tree_data_single_dir_returns_data():
     i = InputData('src/', None, False)
-    gen = i.get_tree_data()
+    gen = read_input_tree(i.tree_input)
     assert list(gen) == [TreeData(1, 0, True, 'src', '')]
 
 
 def test_get_tree_data_file_in_dir_returns_data():
     i = InputData('src/\n  data.txt', None, False)
-    gen = i.get_tree_data()
+    gen = read_input_tree(i.tree_input)
     assert list(gen) == [TreeData(1, 0, True, 'src', ''), TreeData(2, 1, False, 'data.txt', '')]
