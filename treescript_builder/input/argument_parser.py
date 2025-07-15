f2f2b81d9b6700d6e7ee0fe316700fbef8a7191e
DK96-OS <69859316+dk96-os@users.noreply.github.com>
2025-07-15 04:19:54 +0200

    TreeScript Builder Input Package: * Update line_reader - fix lint * Update file_validation - fix lint * Update argument_parser - fix lint

--- treescript_builder/input/argument_parser.py
+++ treescript_builder/input/argument_parser.py
@@ -25,7 +25,7 @@ def parse_arguments(
         exit("No Arguments given.")
     try: # Initialize the Parser and Parse Immediately
         parsed_args = _define_arguments().parse_args(args)
-    except SystemExit as e:
+    except SystemExit:
         exit("Unable to Parse Arguments.")
     return _validate_arguments(
         parsed_args.tree_file_name,
