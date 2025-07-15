94861911509eec92c61fa96d1e124f3a31fbb458
DK96-OS <69859316+dk96-os@users.noreply.github.com>
2025-07-15 04:51:16 +0200

    TreeScript Builder Input Package: * Update input_data - implement FileModeEnum instead of append and prepend fields * Update __init__ - update InputData with FileModeEnum, add hidden method _determine_file_mode_from_arg_data

--- treescript_builder/input/__init__.py
+++ treescript_builder/input/__init__.py
@@ -3,6 +3,8 @@
  - Read Input Tree String from File.
  Author: DK96-OS 2024 - 2025
 """
+from treescript_builder.data.file_mode_enum import FileModeEnum
+from treescript_builder.input.argument_data import ArgumentData
 from treescript_builder.input.argument_parser import parse_arguments
 from treescript_builder.input.file_validation import validate_input_file, validate_directory
 from treescript_builder.input.input_data import InputData
@@ -25,6 +27,15 @@ def validate_input_arguments(arguments: list[str]) -> InputData:
         tree_input=validate_input_file(arg_data.input_file_path_str),
         data_dir=validate_directory(arg_data.data_dir_path_str),
         is_reversed=arg_data.is_reversed,
-        append=not arg_data.overwrite and not arg_data.prepend,
-        prepend=arg_data.prepend,
+        mode=_determine_file_mode_from_arg_data(arg_data),
     )
+
+
+def _determine_file_mode_from_arg_data(
+    arg_data: ArgumentData,
+) -> FileModeEnum:
+    if arg_data.prepend:
+        return FileModeEnum.PREPEND
+    elif arg_data.overwrite:
+        return FileModeEnum.OVERWRITE
+    return FileModeEnum.APPEND
--- treescript_builder/input/input_data.py
+++ treescript_builder/input/input_data.py
@@ -4,6 +4,8 @@
 from dataclasses import dataclass
 from pathlib import Path
 
+from treescript_builder.data.file_mode_enum import FileModeEnum
+
 
 @dataclass(frozen=True)
 class InputData:
@@ -13,11 +15,9 @@ class InputData:
  - tree_input (str): The Tree Input to the FTB operation.
  - data_dir (Path?): An Optional Path to the Data Directory. Default: None
  - is_reversed (bool): Whether this FTB operation is reversed. Default: False
- - append (bool): Whether to append data to the end of files. Default: True
- - prepend (bool): Whether to insert data at the start of files. Default: False.
+ - mode (FileModeEnum): The Mode of File Operation. Default: FileModeEnum.APPEND. Other options: PREPEND, OVERWRITE.
     """
     tree_input: str
     data_dir: Path | None = None
     is_reversed: bool = False
-    append: bool = True
-    prepend: bool = False
+    mode: FileModeEnum = FileModeEnum.APPEND
