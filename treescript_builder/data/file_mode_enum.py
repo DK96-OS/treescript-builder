1be7015e4391eccfe4873a14fd049b739ee87912
DK96-OS <69859316+dk96-os@users.noreply.github.com>
2025-07-22 20:19:56 +0200

    TreeScript Builder Data Package: * Update file_mode_enum - add CANCEL

--- treescript_builder/data/file_mode_enum.py
+++ treescript_builder/data/file_mode_enum.py
@@ -8,9 +8,11 @@ class FileModeEnum(IntEnum):
 
 **Options:**
  - APPEND (default): Add data to the end of files in the operation.
+ - CANCEL: Prevent overwriting by cancelling any file operation necessary.
  - PREPEND: Insert data at the start of each file in the operation.
  - OVERWRITE: Write over all files in the operation with the data.
     """
     APPEND = auto()
+    CANCEL = auto()
     PREPEND = auto()
     OVERWRITE = auto()
