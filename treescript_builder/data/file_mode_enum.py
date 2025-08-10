5b2a861128358154b5806b7ce3954b15c88cadc3
DK96-OS <69859316+dk96-os@users.noreply.github.com>
2025-08-10 04:33:59 +0200

    TreeScript Builder Data Package: * Update file_mode_enum - add MOVE option

--- treescript_builder/data/file_mode_enum.py
+++ treescript_builder/data/file_mode_enum.py
@@ -8,11 +8,13 @@ class FileModeEnum(IntEnum):
 
 **Options:**
  - APPEND (default): Add data to the end of files in the operation.
- - CANCEL: Prevent overwriting by cancelling any file operation necessary.
+ - CANCEL: Prevent overwriting by cancelling any individual copy operation, without stopping the build process.
  - PREPEND: Insert data at the start of each file in the operation.
  - OVERWRITE: Write over all files in the operation with the data.
+ - MOVE: Move files (rename paths) instead of copying data.
     """
     APPEND = auto()
     CANCEL = auto()
     PREPEND = auto()
     OVERWRITE = auto()
+    MOVE = auto()
