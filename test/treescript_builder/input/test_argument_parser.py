71deed099cc21facd63971f43d161f11fbeb61ac
DK96-OS <69859316+dk96-os@users.noreply.github.com>
2025-08-27 04:12:14 +0200

    Test Input Package: * Remove test_input_data * Update test_argument_parser - revise test cases for new argument_parser interface

--- test/treescript_builder/input/test_argument_parser.py
+++ test/treescript_builder/input/test_argument_parser.py
@@ -2,16 +2,13 @@
 """
 import pytest
 
-from treescript_builder.input import parse_arguments, argument_parser
+from treescript_builder.data.input_data import TextModeEnum
+from treescript_builder.input.argument_parser import parse_arguments, _INVALID_ARGUMENTS_COMBINATION_STR, \
+    _INVALID_CONTROL_MODE_ARGUMENTS_STR
 from treescript_builder.input.argument_data import ArgumentData
 
 
 _TS_FILE_NAME = "tree_file"
-
-
-def test_parse_arguments_empty_list_raises_exit():
-    with pytest.raises(SystemExit, match='The TreeScript file path argument is required.'):
-        parse_arguments([])
 
 
 def test_parse_arguments_none_raises_typeerror():
@@ -24,14 +21,19 @@ def test_parse_arguments_int_raises_typeerror():
         parse_arguments(2)
 
 
+def test_parse_arguments_empty_list_raises_exit():
+    with pytest.raises(SystemExit, match='The TreeScript file path argument is required.'):
+        parse_arguments([])
+
+
 @pytest.mark.parametrize(
     "test_input",
     [
-        ([""]),
-        ([" "]),
-        (["--data"]),
-        (["-f"]),
-        ([_TS_FILE_NAME, "--data_dir="]),
+        ([""]),                             # Empty Str
+        ([" "]),                            # Single space char
+        (["--data"]),                       # missing positional argument
+        (["-f"]),                           # This is not a valid flag + Missing positional argument
+        ([_TS_FILE_NAME, "--data_dir="]),   # missing DataDir argument
     ]
 )
 def test_parse_arguments_raises_system_exit(test_input):
@@ -40,176 +42,138 @@ def test_parse_arguments_raises_system_exit(test_input):
 
 
 @pytest.mark.parametrize(
-    "test_input, bool_cancel, bool_move, bool_overwrite, bool_prepend",
-    [
-        # No DataDir, Just Shorthand Options
-        ([_TS_FILE_NAME], False, False, False, False),
-        ([_TS_FILE_NAME, "-c"], True, False, False, False),
-        ([_TS_FILE_NAME, "-m"], False, True, False, False),
-        ([_TS_FILE_NAME, "-o"], False, False, True, False),
-        ([_TS_FILE_NAME, "-p"], False, False, False, True),
+    'test_input, expected_text_append, expected_text_prepend', [
+        ([_TS_FILE_NAME], False, False),                # Default Text Mode
+        ([_TS_FILE_NAME, '--append'], True, False),     # Append Text Mode
+        ([_TS_FILE_NAME, '--prepend'], False, True),    # Prepend Text Mode
+        # Shortcut arguments.
+        ([_TS_FILE_NAME, '--app'], True, False),        # Append Text Mode Short
+        ([_TS_FILE_NAME, '--pre'], False, True),        # Prepend Text Mode Short
     ]
 )
-def test_parse_arguments_returns_data(test_input, bool_cancel, bool_move, bool_overwrite, bool_prepend):
-    assert parse_arguments(test_input) == ArgumentData(
-        input_file_path_str=_TS_FILE_NAME,
-        data_dir_path_str=None,
-        is_reversed=False,
-        verbosity=0,
-        cancel=bool_cancel,
-        move=bool_move,
-        prepend=bool_prepend,
-        overwrite=bool_overwrite,
-    )
-
-
-@pytest.mark.parametrize(
-    "test_input, bool_cancel, bool_move, bool_overwrite, bool_prepend",
-    [
-        ([_TS_FILE_NAME, "-r"], False, False, False, False),
-        ([_TS_FILE_NAME, "-cr"], True, False, False, False),
-        ([_TS_FILE_NAME, "-mr"], False, True, False, False),
-        ([_TS_FILE_NAME, "-or"], False, False, True, False),
-        ([_TS_FILE_NAME, "-pr"], False, False, False, True),
-    ]
-)
-def test_parse_arguments_reverse_shorthand_options_returns_data(
-    test_input, bool_cancel, bool_move, bool_overwrite, bool_prepend
+def test_parse_arguments_defaults_parametrized_text_modes_valid(
+    test_input, expected_text_append, expected_text_prepend
 ):
     assert parse_arguments(test_input) == ArgumentData(
         input_file_path_str=_TS_FILE_NAME,
         data_dir_path_str=None,
-        is_reversed=True,
-        verbosity=0,
-        cancel=bool_cancel,
-        move=bool_move,
-        prepend=bool_prepend,
-        overwrite=bool_overwrite,
+        text_append=expected_text_append,
+        text_prepend=expected_text_prepend,
     )
 
 
 @pytest.mark.parametrize(
-    "test_input, bool_cancel, bool_move, bool_overwrite, bool_prepend",
-    [
-        # Data Dir, And Shorthand Options
-        ([_TS_FILE_NAME, "--data_dir=data"], False, False, False, False),
-        ([_TS_FILE_NAME, "--data_dir=data", "-c"], True, False, False, False),
-        ([_TS_FILE_NAME, "--data_dir=data", "-m"], False, True, False, False),
-        ([_TS_FILE_NAME, "--data_dir=data", "-o"], False, False, True, False),
-        ([_TS_FILE_NAME, "--data_dir=data", "-p"], False, False, False, True),
+    'test_input, expected_text_mode, expect_trim, expect_move', [
+        # Prepend TextMode
+        ([_TS_FILE_NAME, "--data_dir=data", '--prepend'], TextModeEnum.PREPEND, False, False),
+        ([_TS_FILE_NAME, "--data_dir=data", '--prepend', '--move'], TextModeEnum.PREPEND, False, True),
+        ([_TS_FILE_NAME, "--data_dir=data", '--prepend', '-m'], TextModeEnum.PREPEND, False, True),
+        # Append TextMode
+        ([_TS_FILE_NAME, "--data_dir=data", '--append'], TextModeEnum.APPEND, False, False),
+        ([_TS_FILE_NAME, "--data_dir=data", '--append', '--move'], TextModeEnum.APPEND, False, True),
+        ([_TS_FILE_NAME, "--data_dir=data", '--append', '-m'], TextModeEnum.APPEND, False, True),
+        # Prepend w/ Trim
+        ([_TS_FILE_NAME, "--data_dir=data", '-t', '--prepend'], TextModeEnum.PREPEND, True, False),
+        ([_TS_FILE_NAME, "--data_dir=data", '-mt', '--prepend'], TextModeEnum.PREPEND, True, True),
+        # Append w/ Trim
+        ([_TS_FILE_NAME, "--data_dir=data", '-t', '--append'], TextModeEnum.APPEND, True, False),
+        ([_TS_FILE_NAME, "--data_dir=data", '-mt', '--append'], TextModeEnum.APPEND, True, True),
     ]
 )
-def test_parse_arguments_datadir_returns_data(
-    test_input, bool_cancel, bool_move, bool_overwrite, bool_prepend
+def test_parse_arguments_data_dir_parametrized_text_modes_valid(
+    test_input, expected_text_mode, expect_trim, expect_move,
 ):
     assert parse_arguments(test_input) == ArgumentData(
         input_file_path_str=_TS_FILE_NAME,
         data_dir_path_str='data',
-        is_reversed=False,
-        verbosity=0,
-        cancel=bool_cancel,
-        move=bool_move,
-        prepend=bool_prepend,
-        overwrite=bool_overwrite,
+        trim_tree=expect_trim,
+        move_files=expect_move,
+        text_append=expected_text_mode == TextModeEnum.APPEND,
+        text_prepend=expected_text_mode == TextModeEnum.PREPEND,
     )
 
 
 @pytest.mark.parametrize(
-    "test_input, bool_cancel, bool_move, bool_overwrite, bool_prepend",
-    [
-        # DataDir Reversed, And Shorthand Options
-        ([_TS_FILE_NAME, "--data_dir=data", "-r"], False, False, False, False),
-        ([_TS_FILE_NAME, "--data_dir=data", "-rc"], True, False, False, False),
-        ([_TS_FILE_NAME, "--data_dir=data", "-rm"], False, True, False, False),
-        ([_TS_FILE_NAME, "--data_dir=data", "-ro"], False, False, True, False),
-        ([_TS_FILE_NAME, "--data_dir=data", "-rp"], False, False, False, True),
-        # Also, with split datadir args:
-        ([_TS_FILE_NAME, "--data_dir", "data", "-r"], False, False, False, False),
-        ([_TS_FILE_NAME, "--data_dir", "data", "-rc"], True, False, False, False),
-        ([_TS_FILE_NAME, "--data_dir", "data", "-rm"], False, True, False, False),
-        ([_TS_FILE_NAME, "--data_dir", "data", "-ro"], False, False, True, False),
-        ([_TS_FILE_NAME, "--data_dir", "data", "-rp"], False, False, False, True),
+    'test_input, expect_cancel, expect_continue, expect_validate, expect_overwrite, expect_exact', [
+        ([_TS_FILE_NAME], False, False, False, False, False),                           # Default Text Mode
+        ([_TS_FILE_NAME, '--cancel'], True, False, False, False, False),                # Cancel Control Mode
+        ([_TS_FILE_NAME, '--continue'], False, True, False, False, False),              # Continue Control Mode
+        ([_TS_FILE_NAME, '--validate'], False, False, True, False, False),              # Validate Control Mode
+        ([_TS_FILE_NAME, '--overwrite'], False, False, False, True, False),             # Overwrite Control Mode
+        ([_TS_FILE_NAME, '--overwrite', '--exact'], False, False, False, True, True),   # Overwrite Exact Control Mode
+        # Shortcut arguments.
+        ([_TS_FILE_NAME, '--can'], True, False, False, False, False),                   # Cancel Control Mode
+        ([_TS_FILE_NAME, '--con'], False, True, False, False, False),                   # Continue Control Mode
+        ([_TS_FILE_NAME, '--val'], False, False, True, False, False),                   # Validate Control Mode
+        ([_TS_FILE_NAME, '--over'], False, False, False, True, False),                  # Overwrite Control Mode
+        ([_TS_FILE_NAME, '--over', '--ex'], False, False, False, True, True),           # Overwrite Control Mode
     ]
 )
-def test_parse_arguments_datadir_reversed_returns_data(
-    test_input, bool_cancel, bool_move, bool_overwrite, bool_prepend
+def test_parse_arguments_defaults_parametrized_control_modes_valid(
+    test_input, expect_cancel, expect_continue, expect_validate, expect_overwrite, expect_exact
+):
+    assert parse_arguments(test_input) == ArgumentData(
+        input_file_path_str=_TS_FILE_NAME,
+        data_dir_path_str=None,
+        control_cancel=expect_cancel,
+        control_continue=expect_continue,
+        control_validate=expect_validate,
+        control_overwrite=expect_overwrite,
+        control_exact_build=expect_exact,
+    )
+
+
+@pytest.mark.parametrize(
+    'test_input, expected_trim, expected_move', [
+        ([_TS_FILE_NAME], False, False),                    # Default
+        ([_TS_FILE_NAME, '--trim'], True, False),           # Trim Tree
+        ([_TS_FILE_NAME, '--move'], False, True),           # Move Files
+        ([_TS_FILE_NAME, '--move', '--trim'], True, True),  # Move and Trim 
+        # Shortcut arguments.
+        ([_TS_FILE_NAME, '-t'], True, False),               # Trim Tree Short
+        ([_TS_FILE_NAME, '-m'], False, True),               # Move Files Short
+        ([_TS_FILE_NAME, '-mt'], True, True),               # Move and Trim Short
+    ]
+)
+def test_parse_arguments_defaults_parametrized_modifiers_valid(
+    test_input, expected_trim, expected_move
+):
+    assert parse_arguments(test_input) == ArgumentData(
+        input_file_path_str=_TS_FILE_NAME,
+        data_dir_path_str=None,
+        trim_tree=expected_trim,
+        move_files=expected_move,
+    )
+
+
+@pytest.mark.parametrize(
+    'test_input, expected_trim, expected_move', [
+        ([_TS_FILE_NAME, "--data_dir=data"], False, False),                     # Default
+        ([_TS_FILE_NAME, "--data_dir", "data"], False, False),                  # Default 2
+        #
+        ([_TS_FILE_NAME, "--data_dir=data", '--trim'], True, False),            # Trim Tree
+        ([_TS_FILE_NAME, "--data_dir=data", '-t'], True, False),                # Trim Tree Short
+        #
+        ([_TS_FILE_NAME, "--data_dir=data", '--move'], False, True),            # Move Files
+        ([_TS_FILE_NAME, "--data_dir=data", '-m'], False, True),                # Move Files Short
+        #
+        ([_TS_FILE_NAME, "--data_dir=data", '--move', '--trim'], True, True),   # Move and Trim 
+        ([_TS_FILE_NAME, "--data_dir=data", '-mt'], True, True),                # Move and Trim Short
+    ]
+)
+def test_parse_arguments_data_dir_parametrized_modifiers_valid(
+    test_input, expected_trim, expected_move
 ):
     assert parse_arguments(test_input) == ArgumentData(
         input_file_path_str=_TS_FILE_NAME,
         data_dir_path_str='data',
-        is_reversed=True,
-        verbosity=0,
-        cancel=bool_cancel,
-        move=bool_move,
-        prepend=bool_prepend,
-        overwrite=bool_overwrite,
+        trim_tree=expected_trim,
+        move_files=expected_move,
     )
 
 
 @pytest.mark.parametrize(
-    "test_input,expect",
-    [
-        ([_TS_FILE_NAME, "--reverse"], ArgumentData(_TS_FILE_NAME, None, True, 0, False, False, False, False)),
-        ([_TS_FILE_NAME, "--trim"], ArgumentData(_TS_FILE_NAME, None, True, 0, False, False, False, False)),
-        ([_TS_FILE_NAME, "--overwrite"], ArgumentData(_TS_FILE_NAME, None, False, 0, False, False, True, False)),
-        ([_TS_FILE_NAME, "--prepend"], ArgumentData(_TS_FILE_NAME, None, False, 0, False, False, False, True)),
-        ([_TS_FILE_NAME, "--overwrite", '--reverse'], ArgumentData(_TS_FILE_NAME, None, True, 0, False, False, True, False)),
-        ([_TS_FILE_NAME, "--prepend", '--reverse'], ArgumentData(_TS_FILE_NAME, None, True, 0, False, False, False, True)),
-        ([_TS_FILE_NAME, "--verbosity", '--reverse'], ArgumentData(_TS_FILE_NAME, None, True, 1, False, False, False, False)),
-    ]
-)
-def test_parse_arguments_full_argument_names_returns_data(test_input, expect):
-    assert parse_arguments(test_input) == expect
-
-
-@pytest.mark.parametrize(
-    "test_input", [
-        ([_TS_FILE_NAME, "-rpo"]),
-        ([_TS_FILE_NAME, '--reverse', "-po"]),
-        ([_TS_FILE_NAME, '--reverse', "-po", "--data_dir", "data"]),
-    ]
-)
-def test_parse_arguments_invalid_option_combinations_raises_exit(test_input):
-    with pytest.raises(SystemExit, match='Invalid Option Combination.'):
-        parse_arguments(test_input)
-
-
-@pytest.mark.parametrize(
-    "test_input", [
-        ([_TS_FILE_NAME, "-op"]),
-        ([_TS_FILE_NAME, "-po"]), # <- Reverse order should not matter
-        ([_TS_FILE_NAME, "-co"]), # The rest are unique combinations, sorted alphabetically.
-        ([_TS_FILE_NAME, "-mo"]),
-        ([_TS_FILE_NAME, "-cp"]),
-        ([_TS_FILE_NAME, "-mp"]),
-        ([_TS_FILE_NAME, "-cm"]),
-    ]
-)
-def test_parse_arguments_invalid_file_mode_option_combinations_raises_exit(test_input):
-    from re import escape
-    with pytest.raises(SystemExit, match=escape(argument_parser._INVALID_ARGUMENTS_COMBINATION_STR)):
-        parse_arguments(test_input)
-
-
-@pytest.mark.parametrize(
-    "test_input", [
-        ([_TS_FILE_NAME, "-por"]),
-        ([_TS_FILE_NAME, "-cmr"]),
-        ([_TS_FILE_NAME, "-cmopr"]),
-        ([_TS_FILE_NAME, '--reverse', "-po"]),
-        ([_TS_FILE_NAME, '--reverse', "-cm"]),
-        ([_TS_FILE_NAME, '--trim', "-cm"]),
-        ([_TS_FILE_NAME, '--reverse', "-po", "--data_dir", "data"]),
-        ([_TS_FILE_NAME, '--reverse', "-cm", "--data_dir", "data"]),
-    ]
-)
-def test_parse_arguments_reverse_with_invalid_option_combinations_with_more_args_raises_exit(test_input):
-    with pytest.raises(SystemExit, match='Invalid Option Combination.'):
-        parse_arguments(test_input)
-
-
-@pytest.mark.parametrize(
-    "test_input, expected", [
+    "test_input, expected_verbosity", [
         ([_TS_FILE_NAME, "-v"], 1), # <- Verbose is an Argument that Counts.
         ([_TS_FILE_NAME, "-vv"], 2),
         ([_TS_FILE_NAME, "-vvv"], 2), # <- Additional (beyond max 2) are ignored in validation logic.
@@ -218,5 +182,28 @@ def test_parse_arguments_reverse_with_invalid_option_combinations_with_more_args
         ([_TS_FILE_NAME, "--verbosity", "--verbosity"], 2), # <- Technically valid.
     ]
 )
-def test_parse_arguments_verbosity_returns_argument_data(test_input, expected):
-    assert expected == parse_arguments(test_input).verbosity
+def test_parse_arguments_defaults_parametrized_verbosity_valid(
+    test_input, expected_verbosity
+):
+    assert expected_verbosity == parse_arguments(test_input).verbosity
+
+
+@pytest.mark.parametrize(
+    "test_input", [
+        ([_TS_FILE_NAME, "--cancel", "--overwrite"]),
+        ([_TS_FILE_NAME, '--continue', "--cancel"]),
+        ([_TS_FILE_NAME, '--continue', "--validate"]),
+        # w/ Trim
+        ([_TS_FILE_NAME, '-t', "--cancel", "--overwrite"]),
+        ([_TS_FILE_NAME, '-t', '--cancel', "--continue"]),
+        ([_TS_FILE_NAME, '-t', '--continue', "--validate"]),
+        # w/ DataDir
+        ([_TS_FILE_NAME, '-t', "--cancel", "--overwrite", "--data_dir", "data"]),
+        ([_TS_FILE_NAME, '-t', "--cancel", "--continue", "--data_dir", "data"]),
+        ([_TS_FILE_NAME, '-t', "--continue", "--validate", "--data_dir", "data"]),
+    ]
+)
+def test_parse_arguments_invalid_control_mode_combinations_raises_exit(test_input):
+    from re import escape
+    with pytest.raises(SystemExit, match=escape(_INVALID_CONTROL_MODE_ARGUMENTS_STR)):
+        parse_arguments(test_input)
