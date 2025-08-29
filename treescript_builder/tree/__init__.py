7c5a09f40ec00032baf935f0ee8b5c46dae2765f
DK96-OS <69859316+dk96-os@users.noreply.github.com>
2025-08-29 21:17:00 +0200

    TreeScript Builder Tree Package: * Update __init__ - remove method process_results, add hidden methods for validation and build processes * Remove tree_builder * Remove tree_trimmer * Remove path_operations

--- treescript_builder/tree/__init__.py
+++ treescript_builder/tree/__init__.py
@@ -1,56 +1,52 @@
-"""The Tree Module.
+""" The Tree Package Top-Level Module.
 """
-from treescript_builder.input.input_data import InputData
-from treescript_builder.input.line_reader import read_input_tree
+from treescript_builder.data.input_data import InputData
+from treescript_builder.tree.line_reader import read_input_tree
+from treescript_builder.operations.results import process_build_results, process_trim_results
 
 
-def build_tree(input_data: InputData) -> tuple[bool, ...]:
+def build_tree(input_data: InputData) -> str:
     """ Build The Tree as defined by the InputData.
 
 **Parameters:**
  - input_data (str): The InputData produced by the Input Module.
 
 **Returns:**
- tuple[bool, ...] - The results of each individual Builder operation.
+ str - A result summary string, consistent with the InputData verbosity_level attribute.
 
 **Raises:**
  SystemExit - If a Tree Validation error occurs.
 	"""
-    if input_data.is_reversed:
-        from treescript_builder.tree.trim_validation import validate_trim
-        instructions = validate_trim(
-            read_input_tree(input_data.tree_input),
-            input_data.data_dir
-        )
-        from treescript_builder.tree.tree_trimmer import trim
-        results = trim(instructions, input_data.mode)
-    else:
-        from treescript_builder.tree.build_validation import validate_build
-        instructions = validate_build(
-            read_input_tree(input_data.tree_input),
-            input_data.data_dir
-        )
-        from treescript_builder.tree.tree_builder import build
-        results = build(instructions, input_data.mode)
-    #
-    return results
+    if input_data.trim_tree:
+        return _validate_and_trim(input_data)
+    return _validate_and_build(input_data)
 
 
-def process_results(results: tuple[bool, ...]) -> str:
-    """ Process and Summarize the Results.
+def _validate_and_build(in_data: InputData) -> str:
+    from treescript_builder.tree.build_validation import validate_build
+    instructions = validate_build(
+        read_input_tree(in_data.tree_input),
+        in_data.data_dir
+    )
+    from treescript_builder.operations.file_builder import build
+    return process_build_results(
+        instructions,
+        results_tuple=build(instructions, in_data.mode),
+        file_mode=in_data.mode,
+        verbosity_level=in_data.verbosity_level
+    )
 
-**Parameters:**
- - results (tuple[bool]): A tuple containing the results of the operations.
 
-**Returns:**
- str - A summary of the number of operations that succeeded.
-    """
-    if (length := len(results)) == 0:
-        return 'No operations ran.'
-    if (success := sum(iter(results))) == 0:
-        return f"All {length} operations failed."
-    elif success == length:
-        return f"All {length} operations succeeded."
-    # Compute the Fraction of success operations
-    success_percent = round(100 * success / length, 1)
-    return f"{success} out of {length} operations succeeded: {success_percent}%"
+def _validate_and_trim(in_data: InputData) -> str:
+    from treescript_builder.tree.trim_validation import validate_trim
+    instructions = validate_trim(
+        read_input_tree(in_data.tree_input),
+        in_data.data_dir
+    )
+    from treescript_builder.operations.file_trimmer import trim
+    return process_trim_results(
+        instructions,
+        results_tuple=trim(instructions, in_data.mode),
+        file_mode=in_data.mode,
+        verbosity_level=in_data.verbosity_level
+    )
