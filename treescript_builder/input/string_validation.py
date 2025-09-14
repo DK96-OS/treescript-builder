2e03f73fe85f95b305cd8cd79c4fdd2d0d33810c
DK96-OS <69859316+dk96-os@users.noreply.github.com>
2025-09-14 16:04:41 +0200

    TreeScript Builder Input Package: * Update string_validation - revert change to validate_name method (remove isprintable)

--- treescript_builder/input/string_validation.py
+++ treescript_builder/input/string_validation.py
@@ -9,7 +9,6 @@ def validate_name(argument) -> bool:
     """ Determine whether an argument is a non-empty string.
  - Does not count whitespace.
  - Uses the strip method to remove empty space.
- - Applies the Python isprintable str method.
 
 **Parameters:**
  - argument (str): The given argument, which needs validation.
@@ -20,8 +19,6 @@ def validate_name(argument) -> bool:
     if argument is None or not isinstance(argument, str):
         return False
     elif len(argument := argument.strip()) < 1:
-        return False
-    elif not argument.isprintable():
         return False
     return True
 
