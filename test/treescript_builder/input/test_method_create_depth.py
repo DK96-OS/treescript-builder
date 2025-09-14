ffbec5f8663166f38dc06036a22011dab225ec36
DK96-OS <69859316+dk96-os@users.noreply.github.com>
2025-09-14 15:57:39 +0200

    Test Input Package: * Create test_method_validate_input_arguments * Create test_method_create_depth * Remove test_init - split into test_method modules

---
+++ test/treescript_builder/input/test_method_create_depth.py
@@ -0,0 +1,21 @@
+
+import pytest
+
+from test.treescript_builder.conftest import create_depth
+
+
+@pytest.mark.parametrize(
+    "test_input,expect",
+    [
+        (0, 0),
+        (1, 2),
+        (2, 4),
+        (4, 8),
+    ]
+)
+def test_create_depth_returns_expected(test_input, expect):
+    assert len(create_depth(test_input)) == expect
+
+
+def test_create_depth_negative_numbers_returns_empty():
+    assert '' == create_depth(-1)
--- test/treescript_builder/input/test_method_validate_input_arguments.py
+++ test/treescript_builder/input/test_method_validate_input_arguments.py
@@ -1,27 +1,10 @@
 """ Testing Init Module
 """
 from pathlib import Path
+
 import pytest
 
-from test.treescript_builder.conftest import create_depth
 from treescript_builder.input import validate_input_arguments
-
-
-@pytest.mark.parametrize(
-    "test_input,expect",
-    [
-        (0, 0),
-        (1, 2),
-        (2, 4),
-        (4, 8),
-    ]
-)
-def test_create_depth_returns_expected(test_input, expect):
-    assert len(create_depth(test_input)) == expect
-
-
-def test_create_depth_negative_numbers_returns_empty():
-    assert '' == create_depth(-1)
 
 
 def test_validate_input_arguments_returns_input_data():
