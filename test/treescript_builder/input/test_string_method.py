e299eb6b45e507fedaf213662ab0394f34f98697
DK96-OS <69859316+dk96-os@users.noreply.github.com>
2025-09-14 15:01:25 +0200

    Test Input Package: * Remove test_string_method

--- test/treescript_builder/input/test_string_method.py
+++
@@ -1,9 +0,0 @@
-from test.treescript_builder.input.conftest import generate_basenames, generate_filenames
-from treescript_builder.input.string_validation import validate_data_label
-
-
-def test_method():
-    trials = 100
-    for x in range(trials):
-        for fn in generate_filenames():
-            assert validate_data_label(fn)
