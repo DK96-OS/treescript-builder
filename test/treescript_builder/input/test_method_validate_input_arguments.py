006959a0b6ebed5c6ea684b34c941f56c489991d
DK96-OS <69859316+DK96-OS@users.noreply.github.com>
2025-09-19 23:50:29 +0200

    Release 0.1.4 (#13)
    Bumps [actions/checkout](https://github.com/actions/checkout) from 4 to 5:
    * [Changelog](https://github.com/actions/checkout/blob/main/CHANGELOG.md)
    * [Commits](https://github.com/actions/checkout/compare/v4...v5)
    * [Release notes](https://github.com/actions/checkout/releases)
    
    Bumps [dk96-os/gh-action-sigstore-python](https://github.com/dk96-os/gh-action-sigstore-python) from 3.2.7 to 3.2.9:
    * [Changelog](https://github.com/DK96-OS/gh-action-sigstore-python/blob/main/CHANGELOG.md)
    * [Commits](https://github.com/dk96-os/gh-action-sigstore-python/compare/cf654f54962bafc7152d15da33e1015d8e913e2a...5e93f5ab8e0abc13ba45b307b8d35620b6ebd5c9)
    * [Release notes](https://github.com/dk96-os/gh-action-sigstore-python/releases)
    
    Dependabot Update [actions/setup-python](https://github.com/actions/setup-python) from 5 to 6:
    * [Commits](https://github.com/actions/setup-python/compare/v5...v6)
    * [Release notes](https://github.com/actions/setup-python/releases)
    
    GitHub Workflows:
    * Update ci_run - upgrade pytest 8.4.2, upgrade pytest-cov 6.2.1
    
    Project Root:
    * Update README - update how-to section, add script structure section, add DataLabel section, add example section, update to-do section
    * Update setup.py - version 0.1.4
    
    Pull Requests:
    * Bump actions/checkout from 4 to 5 (#10)
    * Bump dk96-os/gh-action-sigstore-python from 3.2.7 to 3.2.9 (#12)
    * Dependabot Update actions/setup-python from 5 to 6 (#15)
    
    Test Fixtures & Data Providers:
    * Create conftest - add MockPathStat class, add TEST_INPUT_FILE to all mock file trees with the treescript (minus the input file) as contents, add field TEST_DATA_DIR, fix lint, update MockPathStat mode for symlink
    
    Test Input Package:
    * Create test_method_create_depth
    * Create test_method_validate_input_arguments
    * Remove test_init - split into test_method modules
    * Update test_file_validation - add test case, add test cases, create MockPathStat class, debug test case, debug test cases requiring new stat object mocking, fix test case names, move MockPathStat class to TestFixtures, reorganize test cases (avoid large envars in parametrized tests on windows), update filenames test cases, update test cases
    * Update test_method_create_depth - update docs
    * Update test_method_validate_input_arguments - debug test cases, implement MockPathStat, update docs
    * Update test_string_validation - add test cases, reorganize some parametrized test cases for validate_data_label method
    
    Test Main Package:
    * Create test_main
    
    TreeScript Builder Input Package:
    * Update __init__ - add newline
    * Update argument_parser - collapse if branches into a single boolean statement
    * Update file_validation - add exit on input invalid file contents to validate_input_file method, add id_dir check to validate_directory method with exit on fail, add newline, implement file size limit in validate_input_file method, implement symlink disabled in validate_input_file method, move validate_directory method error messages into module fields, simplify method validate_directory (the given argument if not None has already been through the validate_name method), update docs
    * Update string_validation - add isprintable to the validate_name method checklist, add restrictions on 1 and 2 char length DataLabels, debug file size limit, fix lint, reimplement method validate_data_label, refactor error messages into module fields, revert change to validate_name method (remove isprintable), simplify method validate_dir_name, simplify method validate_name, update docs

--- test/treescript_builder/input/test_method_validate_input_arguments.py
+++ test/treescript_builder/input/test_method_validate_input_arguments.py
@@ -1,33 +1,18 @@
-""" Testing Init Module
+""" Testing Input Package Method: validate_input_arguments.
 """
 from pathlib import Path
+
 import pytest
 
-from test.treescript_builder.conftest import create_depth
-from treescript_builder.input import validate_input_arguments
-
-
-@pytest.mark.parametrize(
-    "test_input,expect",
-    [
-        (0, 0),
-        (1, 2),
-        (2, 4),
-        (4, 8),
-    ]
-)
-def test_create_depth_returns_expected(test_input, expect):
-    assert len(create_depth(test_input)) == expect
-
-
-def test_create_depth_negative_numbers_returns_empty():
-    assert '' == create_depth(-1)
+from test.treescript_builder.input.conftest import MockPathStat
+from treescript_builder.input import validate_input_arguments, file_validation
 
 
 def test_validate_input_arguments_returns_input_data():
     tree_input_data = 'src/\n  data.txt'
     with pytest.MonkeyPatch().context() as c:
         c.setattr(Path, 'exists', lambda _: True)
+        c.setattr(Path, 'lstat', lambda _: MockPathStat(file_validation._FILE_SIZE_LIMIT))
         c.setattr(Path, 'read_text', lambda _: tree_input_data)
         #
         result = validate_input_arguments(['tree_input'])
@@ -40,21 +25,23 @@ def test_validate_input_arguments_returns_input_data():
 def test_validate_input_arguments_input_file_does_not_exist_raises_error():
     with pytest.MonkeyPatch().context() as c:
         c.setattr(Path, 'exists', lambda _: False)
+        c.setattr(Path, 'lstat', lambda _: MockPathStat(file_validation._FILE_SIZE_LIMIT))
         with pytest.raises(SystemExit):
             validate_input_arguments(['tree_input'])
 
 
-def test_validate_input_arguments_input_file_is_empty_returns_input_no_data_dir():
+def test_validate_input_arguments_input_file_is_empty_raises_exit():
     with pytest.MonkeyPatch().context() as c:
         c.setattr(Path, 'exists', lambda _: True)
+        c.setattr(Path, 'lstat', lambda _: MockPathStat(file_validation._FILE_SIZE_LIMIT))
         c.setattr(Path, 'read_text', lambda _: '')
-        result = validate_input_arguments(['tree_input'])
-        assert result is not None
-        assert result.data_dir is None
+        with pytest.raises(SystemExit, match=file_validation._FILE_VALIDATION_ERROR_MSG):
+            validate_input_arguments(['tree_input'])
 
 
 def test_validate_input_arguments_unknown_argument_raises_error():
     with pytest.MonkeyPatch().context() as c:
         c.setattr(Path, 'exists', lambda _: True)
+        c.setattr(Path, 'lstat', lambda _: MockPathStat(file_validation._FILE_SIZE_LIMIT))
         with pytest.raises(SystemExit):
             validate_input_arguments(['tree_input', 'random-arg'])
