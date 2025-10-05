db3b5452a826881293f2549452756fa298119df5
DK96-OS <69859316+dk96-os@users.noreply.github.com>
2025-10-05 17:16:18 +0200

    TreeScript Builder Data Package: * Update tree_data - add getter method get_data_label

--- treescript_builder/data/tree_data.py
+++ treescript_builder/data/tree_data.py
@@ -1,4 +1,4 @@
-"""Tree Node DataClass.
+""" Tree Node DataClass.
  Author: DK96-OS 2024 - 2025
 """
 from dataclasses import dataclass
@@ -15,9 +15,18 @@ Fields:
  - name (str): The Name of the Tree Node.
  - data_label (str): The Data Label, may be empty string.
     """
-
     line_number: int
     depth: int
     is_dir: bool
     name: str
     data_label: str = ''
+    
+    def get_data_label(self) -> str:
+        """ Obtain the string DataLabel for this TreeData Node.
+ - Contains the relation between these two Fields: name, data_label.
+ - If the DataLabel is the exclamation point !, then use the name as DataLabel.
+
+**Returns:**
+ str - The DataLabel string to use for this TreeData Node. It may not have been validated.
+        """
+        return self.name if self.data_label == '!' else self.data_label
