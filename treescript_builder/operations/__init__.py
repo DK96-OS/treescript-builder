d32852329a71ccba6f6a33ee565d31d6f78f757f
DK96-OS <69859316+dk96-os@users.noreply.github.com>
2025-10-13 13:10:42 +0200

    TreeScript Builder Operations Package: * Update __init__ - merge method operate_with_results into method tree_operations

--- treescript_builder/operations/__init__.py
+++ treescript_builder/operations/__init__.py
@@ -9,29 +9,8 @@ from treescript_builder.operations import file_operations, results
 def tree_operations(
     input_data: InputData,
     instructions: tuple[InstructionData, ...],
-) -> tuple[bool, ...]:
-    """ Execute Tree Operations using InstructionData and InputData options.
-
-**Parameters:**
- - input_data (InputData): The InputData options provided by the Input Package.
- - instructions (tuple[InstructionData]): The set of Instructions obtained from Tree Validation.
-
-**Returns:**
- tuple[bool, ...] - The collection of tree operation results from the corresponding InstructionData tuple.
-    """
-    return file_operations.build(
-        instructions=instructions,
-        move_files=input_data.move_files,
-        control_mode=input_data.control_mode,
-        is_trim=input_data.trim_tree, 
-    )
-
-
-def operate_with_results(
-    input_data: InputData,
-    instructions: tuple[InstructionData, ...],
 ) -> str:
-    """ Execute Tree Operations, and process the results into a printable string.
+    """ Execute Tree Operations using InstructionData and InputData options, and process the results into a printable string.
 
 **Parameters:**
  - input_data (InputData): The InputData options provided by the Input Package.
@@ -40,12 +19,18 @@ def operate_with_results(
 **Returns:**
  str - The Results, after processing into a printable string.
     """
-    return (results.process_trim_results 
+    return (
+        results.process_trim_results
         if input_data.trim_tree else
         results.process_build_results
     )(
         instructions,
-        tree_operations(input_data, instructions),
+        file_operations.build(
+            instructions=instructions,
+            move_files=input_data.move_files,
+            control_mode=input_data.control_mode,
+            is_trim=input_data.trim_tree,
+        ),
         input_data.control_mode,
         input_data.verbosity_level,
     )
