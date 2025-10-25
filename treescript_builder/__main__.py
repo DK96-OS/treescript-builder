ffc478495c2964a3809291858d8d42e6f7b6fd29
DK96-OS <69859316+dk96-os@users.noreply.github.com>
2025-10-25 01:08:37 -0400

    TreeScript Builder Main Package: * Update __main__ - remove step between input_data and tree_operations

--- treescript_builder/__main__.py
+++ treescript_builder/__main__.py
@@ -7,12 +7,9 @@ def main():
     # Process Arguments with Input Package
     from treescript_builder.input import validate_input_arguments
     input_data = validate_input_arguments(argv[1:])
-    # Validate TreeScript with Tree Package
-    from treescript_builder.tree import validate_tree
-    instructions = validate_tree(input_data)
-    # Build Tree with Operations Package
+    # Validate & Build Tree with Operations Package
     from treescript_builder.operations import tree_operations
-    if 0 < len(result := tree_operations(input_data, instructions)):
+    if 0 < len(result := tree_operations(input_data)):
         print(result) # Non-empty result str.
 
 
